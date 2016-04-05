a1c3f05961d395fb275b29a74be75a3bca713b73
Michael Vandeberg <vandeberg@cryptonomex.com>
2016-04-05 17:29:17 -0400

    Cleaned up logging when syncing the blockchain

--- libraries/app/application.cpp
+++ libraries/app/application.cpp
@@ -346,19 +346,17 @@ namespace detail {
                                 std::vector<fc::uint160_t>& contained_transaction_message_ids) override
       { try {
 
-         /*auto latency = graphene::time::now() - blk_msg.block.timestamp;
-         if (!sync_mode || blk_msg.block.block_num() % 10000 == 0)
+         auto latency = graphene::time::now() - blk_msg.block.timestamp;
+         if (sync_mode && blk_msg.block.block_num() % 10000 == 0)
          {
-            const auto& witness = blk_msg.block.witness(*_chain_db);
-            const auto& witness_account = witness.witness_account(*_chain_db);
             auto last_irr = _chain_db->get_dynamic_global_properties().last_irreversible_block_num;
-            ilog("Got block: #${n} time: ${t} latency: ${l} ms from: ${w}  irreversible: ${i} (-${d})",
+            ilog("Syncing Blockchain --- Got block: #${n} time: ${t} latency: ${l} ms from: ${w}  irreversible: ${i} (-${d})",
                  ("t",blk_msg.block.timestamp)
                  ("n", blk_msg.block.block_num())
                  ("l", (latency.count()/1000))
-                 ("w",witness_account.name)
+                 ("w", blk_msg.block.witness)
                  ("i",last_irr)("d",blk_msg.block.block_num()-last_irr) );
-         }*/
+         }
 
          try {
             // TODO: in the case where this block is valid but on a fork that's too old for us to switch to,
@@ -657,7 +655,7 @@ namespace detail {
           }
           while (low_block_num <= high_block_num);
 
-          idump((synopsis));
+          //idump((synopsis));
           return synopsis;
       } FC_CAPTURE_AND_RETHROW() }
 
