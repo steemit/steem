5211365118d7ac493f30ab6c702c0c977ce823f8
arhag <arhag@users.noreply.github.com>
2016-05-10 19:30:08 -0400

    Revert changes to steem_evaluator.cpp from prior commit

--- libraries/chain/steem_evaluator.cpp
+++ libraries/chain/steem_evaluator.cpp
@@ -122,8 +122,7 @@ void account_update_evaluator::do_apply( const account_update_operation& o )
 
 void comment_evaluator::do_apply( const comment_operation& o )
 { try {
-   if( db().is_producing() || db().has_hardfork( STEEMIT_HARDFORK_0_5_0) ) 
-      FC_ASSERT( o.title.size() + o.body.size() + o.json_metadata.size(), "something should change" );
+   FC_ASSERT( o.title.size() + o.body.size() + o.json_metadata.size(), "something should change" );
 
    const auto& by_permlink_idx = db().get_index_type< comment_index >().indices().get< by_permlink >();
    auto itr = by_permlink_idx.find( boost::make_tuple( o.author, o.permlink ) );
