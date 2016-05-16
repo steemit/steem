4ef97beee78e4a2745da2e336ca6125007c97647
Reverse Flash <dwest.flash@gmail.com>
2016-05-16 18:56:59 -0400

    update state to return proper vote info

--- libraries/app/include/steemit/app/state.hpp
+++ libraries/app/include/steemit/app/state.hpp
@@ -26,11 +26,17 @@ namespace steemit { namespace app {
    struct vote_state {
       string   voter;
       uint64_t weight;
+      int64_t        rshares;
+      int16_t        percent;
+      time_point_sec time;
    };
 
    struct account_vote {
-      string   authorperm;
-      uint64_t weight;
+      string         authorperm;
+      uint64_t       weight;
+      int64_t        rshares;
+      int16_t        percent;
+      time_point_sec time;
    };
 
    struct  discussion : public comment_object {
@@ -144,8 +150,8 @@ FC_REFLECT_DERIVED( steemit::app::extended_account,
                    (transfer_history)(market_history)(post_history)(vote_history)(other_history)(witness_votes)(posts)(blog)(recent_replies)(blog_category) )
 
 
-FC_REFLECT( steemit::app::vote_state, (voter)(weight) );
-FC_REFLECT( steemit::app::account_vote, (authorperm)(weight) );
+FC_REFLECT( steemit::app::vote_state, (voter)(weight)(rshares)(percent)(time) );
+FC_REFLECT( steemit::app::account_vote, (authorperm)(weight)(rshares)(percent)(time) );
 
 FC_REFLECT( steemit::app::discussion_index, (category)(trending)(recent)(active)(votes)(maturing)(best) )
 FC_REFLECT( steemit::app::category_index, (trending)(active)(recent)(best) )
