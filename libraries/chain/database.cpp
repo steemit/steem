efe735047007b33e54c8c086aa28b1a44f2ee553
Michael Vandeberg <vandeberg@steemit.com>
2016-06-24 11:07:14 -0400

    Move witnesses_voted_for reset into database::clear_witness_votes to make that function leave database in consistent state. #104

--- libraries/chain/database.cpp
+++ libraries/chain/database.cpp
@@ -1483,6 +1483,14 @@ void database::clear_witness_votes( const account_object& a )
       ++itr;
       remove(current);
    }
+
+   if( has_hardfork( STEEMIT_HARDFORK_0_6 ) ) // TODO: this check can be removed after hard fork 6
+   {
+      modify( a, [&](account_object& acc )
+      {
+         acc.witnesses_voted_for = 0;
+      });
+   }
 }
 
 /**
