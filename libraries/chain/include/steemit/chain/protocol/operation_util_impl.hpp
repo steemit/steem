8ed14f26148c8c4c7ea4f199bb340e44a869cdbe
Michael Vandeberg <vandeberg@steemit.com>
2016-07-27 15:09:48 -0400

    Cleanup

--- libraries/chain/include/steemit/chain/protocol/operation_util_impl.hpp
+++ libraries/chain/include/steemit/chain/protocol/operation_util_impl.hpp
@@ -14,25 +14,27 @@ namespace fc
    struct from_operation
    {
       variant& var;
-      from_operation( variant& dv ):var(dv){}
+      from_operation( variant& dv )
+         : var( dv ) {}
 
       typedef void result_type;
       template<typename T> void operator()( const T& v )const
       {
-         auto name = name_from_type( fc::get_typename<T>::name() );
-         var = variant( std::make_pair(name,v) );
+         auto name = name_from_type( fc::get_typename< T >::name() );
+         var = variant( std::make_pair( name, v ) );
       }
    };
 
    struct get_operation_name
    {
       string& name;
-      get_operation_name( string& dv ):name(dv){}
+      get_operation_name( string& dv )
+         : name( dv ) {}
 
       typedef void result_type;
-      template<typename T> void operator()( const T& v )const
+      template< typename T > void operator()( const T& v )const
       {
-         name = name_from_type( fc::get_typename<T>::name() );
+         name = name_from_type( fc::get_typename< T >::name() );
       }
    };
 }
@@ -50,18 +52,19 @@ struct operation_get_required_auth_visitor
 {
    typedef void result_type;
 
-   flat_set<string>&    active;
-   flat_set<string>&    owner;
-   flat_set<string>&    posting;
-   std::vector<authority>&   other;
+   flat_set< string >&        active;
+   flat_set< string >&        owner;
+   flat_set< string >&        posting;
+   std::vector< authority >&  other;
 
    operation_get_required_auth_visitor(
-     flat_set<string>& a,
-     flat_set<string>& own,
-     flat_set<string>& post,
-     std::vector<authority>&  oth ):active(a),owner(own),posting(post),other(oth){}
+         flat_set<string>& a,
+         flat_set<string>& own,
+         flat_set<string>& post,
+         std::vector< authority >& oth )
+      : active( a ), owner( own ), posting( post ), other( oth ) {}
 
-   template<typename T>
+   template< typename T >
    void operator()( const T& v )const
    {
       v.get_required_active_authorities( active );
@@ -77,56 +80,58 @@ struct operation_get_required_auth_visitor
 // Place DEFINE_OPERATION_TYPE in a .cpp file to define
 // functions related to your operation type
 //
-#define DEFINE_OPERATION_TYPE( OperationType )                          \
-namespace fc {                                                          \
-void to_variant( const OperationType& var,  fc::variant& vo )           \
-{                                                                       \
-   var.visit( from_operation( vo ) );                                   \
-}                                                                       \
-                                                                        \
-void from_variant( const fc::variant& var,  OperationType& vo )         \
-{                                                                       \
-   static std::map<string,uint32_t> to_tag = []()                       \
-   {                                                                    \
-      std::map<string,uint32_t> name_map;                               \
-      for( int i = 0; i < OperationType::count(); ++i )                 \
-      {                                                                 \
-         OperationType tmp;                                             \
-         tmp.set_which(i);                                              \
-         string n;                                                      \
-         tmp.visit( get_operation_name(n) );                            \
-         name_map[n] = i;                                               \
-      }                                                                 \
-      return name_map;                                                  \
-   }();                                                                 \
-                                                                        \
-   auto ar = var.get_array();                                           \
-   if( ar.size() < 2 ) return;                                          \
-   if( ar[0].is_uint64() )                                              \
-      vo.set_which( ar[0].as_uint64() );                                \
-   else                                                                 \
-   {                                                                    \
-      auto itr = to_tag.find(ar[0].as_string());                        \
+#define DEFINE_OPERATION_TYPE( OperationType )                             \
+namespace fc {                                                             \
+                                                                           \
+void to_variant( const OperationType& var,  fc::variant& vo )              \
+{                                                                          \
+   var.visit( from_operation( vo ) );                                      \
+}                                                                          \
+                                                                           \
+void from_variant( const fc::variant& var,  OperationType& vo )            \
+{                                                                          \
+   static std::map<string,uint32_t> to_tag = []()                          \
+   {                                                                       \
+      std::map<string,uint32_t> name_map;                                  \
+      for( int i = 0; i < OperationType::count(); ++i )                    \
+      {                                                                    \
+         OperationType tmp;                                                \
+         tmp.set_which(i);                                                 \
+         string n;                                                         \
+         tmp.visit( get_operation_name(n) );                               \
+         name_map[n] = i;                                                  \
+      }                                                                    \
+      return name_map;                                                     \
+   }();                                                                    \
+                                                                           \
+   auto ar = var.get_array();                                              \
+   if( ar.size() < 2 ) return;                                             \
+   if( ar[0].is_uint64() )                                                 \
+      vo.set_which( ar[0].as_uint64() );                                   \
+   else                                                                    \
+   {                                                                       \
+      auto itr = to_tag.find(ar[0].as_string());                           \
       FC_ASSERT( itr != to_tag.end(), "Invalid operation name: ${n}", ("n", ar[0]) ); \
-      vo.set_which( to_tag[ar[0].as_string()] );                        \
-   }                                                                    \
-      vo.visit( fc::to_static_variant( ar[1] ) );                       \
-   }                                                                    \
-}                                                                       \
-namespace steemit { namespace chain {                                 \
-                                                                      \
-void operation_validate( const OperationType& op )                    \
-{                                                                     \
-   op.visit( steemit::chain::operation_validate_visitor() );          \
-}                                                                     \
-                                                                      \
-void operation_get_required_authorities( const OperationType& op,     \
-                                         flat_set<string>& active,    \
-                                         flat_set<string>& owner,     \
-                                         flat_set<string>& posting,   \
-                                         std::vector<authority>& other ) \
-{                                                                     \
+      vo.set_which( to_tag[ar[0].as_string()] );                           \
+   }                                                                       \
+      vo.visit( fc::to_static_variant( ar[1] ) );                          \
+   }                                                                       \
+}                                                                          \
+                                                                           \
+namespace steemit { namespace chain {                                      \
+                                                                           \
+void operation_validate( const OperationType& op )                         \
+{                                                                          \
+   op.visit( steemit::chain::operation_validate_visitor() );               \
+}                                                                          \
+                                                                           \
+void operation_get_required_authorities( const OperationType& op,          \
+                                         flat_set<string>& active,         \
+                                         flat_set<string>& owner,          \
+                                         flat_set<string>& posting,        \
+                                         std::vector<authority>& other )   \
+{                                                                          \
    op.visit( steemit::chain::operation_get_required_auth_visitor( active, owner, posting, other ) ); \
-}                                                                     \
-                                                                      \
+}                                                                          \
+                                                                           \
 } } /* steemit::chain */
--- libraries/chain/protocol/operation_util_impl.cpp
+++ libraries/chain/protocol/operation_util_impl.cpp
@@ -4,7 +4,7 @@ namespace fc {
 
 std::string name_from_type( const std::string& type_name )
 {
-   auto start = type_name.find_last_of( ':' )+1;
+   auto start = type_name.find_last_of( ':' ) + 1;
    auto end   = type_name.find_last_of( '_' );
    return type_name.substr( start, end-start );
 }
