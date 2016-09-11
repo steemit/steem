09701383afe607739d0274dfcfe1043af8e28377
abitmore <abitmore@users.noreply.github.com>
2016-09-11 09:32:31 +0000

    Fix clear_expired_transactions memory leak issue #265

--- libraries/chain/database.cpp
+++ libraries/chain/database.cpp
@@ -3432,8 +3432,8 @@ void database::clear_expired_transactions()
    //Transactions must have expired by at least two forking windows in order to be removed.
    auto& transaction_idx = static_cast<transaction_index&>(get_mutable_index(implementation_ids, impl_transaction_object_type));
    const auto& dedupe_index = transaction_idx.indices().get<by_expiration>();
-   while( (!dedupe_index.empty()) && (head_block_time() > dedupe_index.rbegin()->trx.expiration) )
-      transaction_idx.remove(*dedupe_index.rbegin());
+   while( (!dedupe_index.empty()) && (head_block_time() > dedupe_index.begin()->trx.expiration) )
+      transaction_idx.remove(*dedupe_index.begin());
 }
 
 void database::clear_expired_orders()
