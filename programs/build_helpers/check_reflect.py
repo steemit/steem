b228bb908a22b745192a5b7baab21d5ad7cb84a8
theoreticalbts <theoreticalbts@users.noreply.github.com>
2016-09-13 11:47:20 -0400

    check_reflect.py:  sys.exit(1) on FC_REFLECT(object)

--- programs/build_helpers/check_reflect.py
+++ programs/build_helpers/check_reflect.py
@@ -6,6 +6,8 @@ import re
 import sys
 import xml.etree.ElementTree as etree
 
+other_issues = []
+
 def process_node(path, node):
     """
     if node.tag == "TestCase":
@@ -89,7 +91,8 @@ for root, dirs, files in os.walk("."):
                     members = bubble_list(m.group(2))
                     name2members_re[cname] = members
                     if cname.endswith("_object"):
-                       print("FC_REFLECT on {} should be FC_REFLECT_DERIVED".format(cname))
+                        other_issues.append("FC_REFLECT on {} should be FC_REFLECT_DERIVED".format(cname))
+                        print(other_issues[-1])
                 for m in re_reflect_derived.finditer(content):
                     cname = m.group(1)
                     members = bubble_list(m.group(3))
@@ -138,11 +141,11 @@ def validate_members(name2members_ref, name2members_test):
     for item in error_items:
         print(item)
 
-    return {"ok_items" : ok_items, "ne_items" : ne_items, "error_items" : error_items}
+    return {"ok_items" : ok_items, "ne_items" : ne_items, "error_items" : error_items, "other_issues" : other_issues}
 
 if __name__ == "__main__":
     result = validate_members(name2members_doxygen, name2members_re)
-    if len(result["error_items"]) == 0:
+    if len(result["error_items"]) + len(result["other_issues"]) == 0:
         exit_code = 0
     else:
         exit_code = 1
