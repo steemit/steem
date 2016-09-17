d3da2212de287d1cb240df739dffb331583462a9
Krzysztof Szumny <noisy.pl@gmail.com>
2016-09-17 22:34:15 +0200

    Error message adjusted to value of STEEMIT_OWNER_UPDATE_LIMIT

--- libraries/chain/steem_evaluator.cpp
+++ libraries/chain/steem_evaluator.cpp
@@ -155,7 +155,7 @@ void account_update_evaluator::do_apply( const account_update_operation& o )
    {
 #ifndef IS_TESTNET
       if( db().has_hardfork( STEEMIT_HARDFORK_0_11 ) )
-         FC_ASSERT( db().head_block_time() - account.last_owner_update > STEEMIT_OWNER_UPDATE_LIMIT, "can only update owner authority once a minute" );
+         FC_ASSERT( db().head_block_time() - account.last_owner_update > STEEMIT_OWNER_UPDATE_LIMIT, "can only update owner authority once an hour" );
 #endif
 
       db().update_owner_authority( account, *o.owner );
