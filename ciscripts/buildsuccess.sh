ec429559542e9fad1e9ad0d01bbf4d489710656c
JustinW <juswelch@gmail.com>
2016-12-04 19:23:24 -0500

    testcommit

--- ciscripts/buildsuccess.sh
+++ ciscripts/buildsuccess.sh
@@ -1,4 +1,5 @@
 #/bin/bash
+#testcommit
 curl -XPOST -H "Authorization: token $GITHUB_SECRET" https://api.github.com/repos/steemit/steem/statuses/$(git rev-parse HEAD) -d "{
   \"state\": \"success\",
   \"target_url\": \"${BUILD_URL}\",
