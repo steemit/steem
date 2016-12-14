3fe01234efd2b924a2f834ab2df9a537a719e1b1
JustinW <juswelch@gmail.com>
2016-12-14 17:18:21 -0500

    Added tagging for latest image on dockerhub if the branchname is named stable, added build context for github status (so that it does't report as 'default'), and now removing exited docker containers.

--- ciscripts/buildscript.sh
+++ ciscripts/buildscript.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 export IMAGE_NAME="steemit/steem:${GIT_BRANCH#*/}"
+if [[ $IMAGE_NAME == "steemit/steem:stable" ]] ; then
+  IMAGE_NAME="steemit/steem:latest"
+fi
 sudo docker build -t=$IMAGE_NAME .
 sudo docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 sudo docker push $IMAGE_NAME
