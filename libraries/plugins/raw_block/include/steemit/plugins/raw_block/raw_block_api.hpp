f9200d3cc694c62b3315ef802826aebee9b8a3aa
Nate Brune <natebrune@gmail.com>
2016-12-15 16:00:16 -0500

    Add doxygen comments to various plugin apis

--- libraries/plugins/raw_block/include/steemit/plugins/raw_block/raw_block_api.hpp
+++ libraries/plugins/raw_block/include/steemit/plugins/raw_block/raw_block_api.hpp
@@ -28,14 +28,27 @@ struct get_raw_block_result
    std::string                   raw_block;
 };
 
+/**
+ * @brief Allows for unserialized block data to be retrevied.
+ */
 class raw_block_api
 {
    public:
       raw_block_api( const steemit::app::api_context& ctx );
 
       void on_api_startup();
-
+      /**
+       * @brief Allows for unserialized block data to be retrevied.
+       * @param args @ref get_raw_block_args object which simply contains which block you wish to recieve.
+       * @returns @ref get_raw_block_result which contains the raw block as a string.
+       */
       get_raw_block_result get_raw_block( get_raw_block_args args );
+
+      /**
+       * @brief Allows for unserialized block data to be broadcast/
+       * @param block_b64 string that contains signed block data.
+       * @returns void
+       */
       void push_raw_block( std::string block_b64 );
 
    private:
