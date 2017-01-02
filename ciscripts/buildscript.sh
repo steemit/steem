3365063a0045bb0c05cad61653bd068bc3f2c558
JustinW <juswelch@gmail.com>
2017-01-02 22:10:22 +0000

    cobertura plugin wants cobertura.xml in a path relative to the workspace

--- ciscripts/buildscript.sh
+++ ciscripts/buildscript.sh
@@ -7,3 +7,4 @@ sudo docker build -t=$IMAGE_NAME .
 sudo docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 sudo docker push $IMAGE_NAME
 sudo docker run -v /var/jenkins_home:/var/jenkins $IMAGE_NAME cp -r /var/cobertura /var/jenkins
+cp -r /var/jenkins_home/cobertura .
