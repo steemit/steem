82b92a79a3cb58933ba4df4439cb619b8f288ff2
Mikhail Komarov <nemo1369@gmail.com>
2017-01-19 10:52:17 +0300

    Docker configuration updated to enable even more plugins. Docker golos daemon service updated. RPC an P2P endpoints made configurable by enviroment variables

--- contrib/golosd.sh
+++ contrib/golosd.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-export HOME="/var/lib/steemd"
+export HOME="/var/lib/golosd"
 
-STEEMD="/usr/local/steemd-default/bin/steemd"
+STEEMD="/usr/local/bin/golosd"
 
-if [[ "$USE_WAY_TOO_MUCH_RAM" ]]; then
-    STEEMD="/usr/local/steemd-full/bin/steemd"
-fi
+#if [[ "$USE_WAY_TOO_MUCH_RAM" ]]; then
+#    STEEMD="/usr/local/golosd-full/bin/golosd"
+#fi
 
-chown -R steemd:steemd $HOME
+chown -R golosd:golosd $HOME
 
 # seed nodes come from doc/seednodes which is
-# installed by docker into /etc/steemd/seednodes
-SEED_NODES="$(cat /etc/steemd/seednodes | awk -F' ' '{print $1}')"
+# installed by docker into /etc/golosd/seednodes
+# SEED_NODES="$(cat /etc/golosd/seednodes | awk -F' ' '{print $1}')"
 
 ARGS=""
 
@@ -46,18 +46,18 @@ if [[ ! -z "$STEEMD_PRIVATE_KEY" ]]; then
 fi
 
 # overwrite local config with image one
-cp /etc/steemd/config.ini $HOME/config.ini
+cp /etc/golosd/config.ini $HOME/config.ini
 
-chown steemd:steemd $HOME/config.ini
+chown golosd:golosd $HOME/config.ini
 
 if [[ ! -d $HOME/blockchain ]]; then
-    if [[ -e /var/cache/steemd/blocks.tbz2 ]]; then
+    if [[ -e /var/cache/golosd/blocks.tbz2 ]]; then
         # init with blockchain cached in image
         ARGS+=" --replay-blockchain"
         mkdir -p $HOME/blockchain/database
         cd $HOME/blockchain/database
-        tar xvjpf /var/cache/steemd/blocks.tbz2
-        chown -R steemd:steemd $HOME/blockchain
+        tar xvjpf /var/cache/golosd/blocks.tbz2
+        chown -R golosd:golosd $HOME/blockchain
     fi
 fi
 
@@ -68,10 +68,22 @@ cd $HOME
 # slow down restart loop if flapping
 sleep 1
 
-exec chpst -usteemd \
+if [[ ! -z "$STEEMD_RPC_ENDPOINT" ]]; then
+    RPC_ENDPOINT=$STEEMD_RPC_ENDPOINT
+else
+    RPC_ENDPOINT="0.0.0.0:8090"
+fi
+
+if [[ ! -z "$STEEMD_P2P_ENDPOINT" ]]; then
+    P2P_ENDPOINT=$STEEMD_P2P_ENDPOINT
+else
+    P2P_ENDPOINT="0.0.0.0:2001"
+fi
+
+exec chpst -ugolosd \
     $STEEMD \
-        --rpc-endpoint=0.0.0.0:8090 \
-        --p2p-endpoint=0.0.0.0:2001 \
+        --rpc-endpoint=${RPC_ENDPOINT} \
+        --p2p-endpoint=${P2P_ENDPOINT} \
         --data-dir=$HOME \
         $ARGS \
         $STEEMD_EXTRA_OPTS \
