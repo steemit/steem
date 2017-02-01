99a0ee0ccb8f1ddb31ef24be3827e9a0c57ce837
JustinW <juswelch@gmail.com>
2017-02-01 22:05:02 +0000

    move build pending into buildscript.sh

--- ciscripts/buildscript.sh
+++ ciscripts/buildscript.sh
@@ -9,6 +9,12 @@ export BUILD_SPACE=`mktemp -d`
 cp -a temp-repo-folder/. $BUILD_SPACE
 rm -rf temp-repo-folder/*
 cd $BUILD_SPACE
+curl -XPOST -H "Authorization: token $GITHUB_SECRET" https://api.github.com/repos/steemit/steem/statuses/$(git rev-parse HEAD) -d "{
+  \"state\": \"pending\",
+  \"target_url\": \"${BUILD_URL}\",
+  \"description\": \"The build is now pending in jenkinsci!\",
+  \"context\": \"jenkins-ci-steemit\"
+}"
 sudo docker build -t=$IMAGE_NAME .
 sudo docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 sudo docker push $IMAGE_NAME
