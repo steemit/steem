12cf36e971695b1595179dde292e2f5a65c0df1c
JustinW <juswelch@gmail.com>
2017-02-04 02:11:51 +0000

    quiet down curl and test webhook for multibranch

--- ciscripts/buildfailure.sh
+++ ciscripts/buildfailure.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -XPOST -H "Authorization: token $GITHUB_SECRET" https://api.github.com/repos/steemit/steem/statuses/$(git rev-parse HEAD) -d "{
+curl --silent -XPOST -H "Authorization: token $GITHUB_SECRET" https://api.github.com/repos/steemit/steem/statuses/$(git rev-parse HEAD) -d "{
   \"state\": \"failure\",
   \"target_url\": \"${BUILD_URL}\",
   \"description\": \"JenkinsCI reports the build has failed!\",
--- ciscripts/buildsuccess.sh
+++ ciscripts/buildsuccess.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 #testcommit
-curl -XPOST -H "Authorization: token $GITHUB_SECRET" https://api.github.com/repos/steemit/steem/statuses/$(git rev-parse HEAD) -d "{
+curl --silent -XPOST -H "Authorization: token $GITHUB_SECRET" https://api.github.com/repos/steemit/steem/statuses/$(git rev-parse HEAD) -d "{
   \"state\": \"success\",
   \"target_url\": \"${BUILD_URL}\",
   \"description\": \"Jenkins-CI reports build succeeded!!\",
