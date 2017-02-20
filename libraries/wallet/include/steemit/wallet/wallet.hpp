2ea7ba29335ae7350ffe3aea51c398822a52c9d4
Michael Vandeberg <vandeberg@steemit.com>
2017-02-20 15:40:37 -0500

    Restore existing create_account APIs and create new APIs for creation with delegation

--- libraries/wallet/include/steemit/wallet/wallet.hpp
+++ libraries/wallet/include/steemit/wallet/wallet.hpp
@@ -361,7 +361,7 @@ class wallet_api
        *  @param json_meta JSON Metadata associated with the new account
        *  @param broadcast true if you wish to broadcast the transaction
        */
-      annotated_signed_transaction create_account( string creator, string new_account_name, string json_meta, asset steem_fee, asset delegated_vests, bool broadcast );
+      annotated_signed_transaction create_account( string creator, string new_account_name, string json_meta, bool broadcast );
 
       /**
        * This method is used by faucets to create new accounts for other users which must
@@ -381,8 +381,53 @@ class wallet_api
       annotated_signed_transaction create_account_with_keys( string creator,
                                             string newname,
                                             string json_meta,
+                                            public_key_type owner,
+                                            public_key_type active,
+                                            public_key_type posting,
+                                            public_key_type memo,
+                                            bool broadcast )const;
+
+      /**
+       *  This method will genrate new owner, active, and memo keys for the new account which
+       *  will be controlable by this wallet. There is a fee associated with account creation
+       *  that is paid by the creator. The current account creation fee can be found with the
+       *  'info' wallet command.
+       *
+       *  These accounts are created with combination of STEEM and delegated SP
+       *
+       *  @param creator The account creating the new account
+       *  @param steem_fee The amount of the fee to be paid with STEEM
+       *  @param delegated_vests The amount of the fee to be paid with delegation
+       *  @param new_account_name The name of the new account
+       *  @param json_meta JSON Metadata associated with the new account
+       *  @param broadcast true if you wish to broadcast the transaction
+       */
+      annotated_signed_transaction create_account_delegated( string creator, asset steem_fee, asset delegated_vests, string new_account_name, string json_meta, bool broadcast );
+
+      /**
+       * This method is used by faucets to create new accounts for other users which must
+       * provide their desired keys. The resulting account may not be controllable by this
+       * wallet. There is a fee associated with account creation that is paid by the creator.
+       * The current account creation fee can be found with the 'info' wallet command.
+       *
+       * These accounts are created with combination of STEEM and delegated SP
+       *
+       * @param creator The account creating the new account
+       * @param steem_fee The amount of the fee to be paid with STEEM
+       * @param delegated_vests The amount of the fee to be paid with delegation
+       * @param newname The name of the new account
+       * @param json_meta JSON Metadata associated with the new account
+       * @param owner public owner key of the new account
+       * @param active public active key of the new account
+       * @param posting public posting key of the new account
+       * @param memo public memo key of the new account
+       * @param broadcast true if you wish to broadcast the transaction
+       */
+      annotated_signed_transaction create_account_with_keys_delegated( string creator,
                                             asset steem_fee,
                                             asset delegated_vests,
+                                            string newname,
+                                            string json_meta,
                                             public_key_type owner,
                                             public_key_type active,
                                             public_key_type posting,
@@ -991,6 +1036,8 @@ FC_API( steemit::wallet::wallet_api,
         /// transaction api
         (create_account)
         (create_account_with_keys)
+        (create_account_delegated)
+        (create_account_with_keys_delegated)
         (update_account)
         (update_account_auth_key)
         (update_account_auth_account)
--- libraries/wallet/wallet.cpp
+++ libraries/wallet/wallet.cpp
@@ -1294,8 +1294,40 @@ feed_history_api_obj wallet_api::get_feed_history()const { return my->_remote_db
 annotated_signed_transaction wallet_api::create_account_with_keys( string creator,
                                       string new_account_name,
                                       string json_meta,
+                                      public_key_type owner,
+                                      public_key_type active,
+                                      public_key_type posting,
+                                      public_key_type memo,
+                                      bool broadcast )const
+{ try {
+   FC_ASSERT( !is_locked() );
+   account_create_operation op;
+   op.creator = creator;
+   op.new_account_name = new_account_name;
+   op.owner = authority( 1, owner, 1 );
+   op.active = authority( 1, active, 1 );
+   op.posting = authority( 1, posting, 1 );
+   op.memo_key = memo;
+   op.json_metadata = json_meta;
+   op.fee = my->_remote_db->get_chain_properties().account_creation_fee * asset( STEEMIT_CREATE_ACCOUNT_WITH_STEEM_MODIFIER, STEEM_SYMBOL );
+
+   signed_transaction tx;
+   tx.operations.push_back(op);
+   tx.validate();
+
+   return my->sign_transaction( tx, broadcast );
+} FC_CAPTURE_AND_RETHROW( (creator)(new_account_name)(json_meta)(owner)(active)(memo)(broadcast) ) }
+
+/**
+ * This method is used by faucets to create new accounts for other users which must
+ * provide their desired keys. The resulting account may not be controllable by this
+ * wallet.
+ */
+annotated_signed_transaction wallet_api::create_account_with_keys_delegated( string creator,
                                       asset steem_fee,
                                       asset delegated_vests,
+                                      string new_account_name,
+                                      string json_meta,
                                       public_key_type owner,
                                       public_key_type active,
                                       public_key_type posting,
@@ -1660,7 +1692,7 @@ annotated_signed_transaction wallet_api::delegate_vesting_shares( string delegat
  *  This method will genrate new owner, active, and memo keys for the new account which
  *  will be controlable by this wallet.
  */
-annotated_signed_transaction wallet_api::create_account( string creator, string new_account_name, string json_meta, asset steem_fee, asset delegated_vests, bool broadcast )
+annotated_signed_transaction wallet_api::create_account( string creator, string new_account_name, string json_meta, bool broadcast )
 { try {
    FC_ASSERT( !is_locked() );
    auto owner = suggest_brain_key();
@@ -1671,7 +1703,25 @@ annotated_signed_transaction wallet_api::create_account( string creator, string 
    import_key( active.wif_priv_key );
    import_key( posting.wif_priv_key );
    import_key( memo.wif_priv_key );
-   return create_account_with_keys( creator, new_account_name, json_meta, steem_fee, delegated_vests, owner.pub_key, active.pub_key, posting.pub_key, memo.pub_key, broadcast );
+   return create_account_with_keys( creator, new_account_name, json_meta, owner.pub_key, active.pub_key, posting.pub_key, memo.pub_key, broadcast );
+} FC_CAPTURE_AND_RETHROW( (creator)(new_account_name)(json_meta) ) }
+
+/**
+ *  This method will genrate new owner, active, and memo keys for the new account which
+ *  will be controlable by this wallet.
+ */
+annotated_signed_transaction wallet_api::create_account_delegated( string creator, asset steem_fee, asset delegated_vests, string new_account_name, string json_meta, bool broadcast )
+{ try {
+   FC_ASSERT( !is_locked() );
+   auto owner = suggest_brain_key();
+   auto active = suggest_brain_key();
+   auto posting = suggest_brain_key();
+   auto memo = suggest_brain_key();
+   import_key( owner.wif_priv_key );
+   import_key( active.wif_priv_key );
+   import_key( posting.wif_priv_key );
+   import_key( memo.wif_priv_key );
+   return create_account_with_keys_delegated( creator, steem_fee, delegated_vests, new_account_name, json_meta,  owner.pub_key, active.pub_key, posting.pub_key, memo.pub_key, broadcast );
 } FC_CAPTURE_AND_RETHROW( (creator)(new_account_name)(json_meta) ) }
 
 
