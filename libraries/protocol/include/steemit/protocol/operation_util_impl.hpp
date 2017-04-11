f8d60a1dde24450c06b84517dc0a8fd4d566163d
theoreticalbts <theoreticalbts@users.noreply.github.com>
2017-04-11 10:35:25 -0400

    Improve indentation, align backslashes in operation_util_impl.hpp

--- libraries/protocol/include/steemit/protocol/operation_util_impl.hpp
+++ libraries/protocol/include/steemit/protocol/operation_util_impl.hpp
@@ -111,27 +111,30 @@ void from_variant( const fc::variant& var,  OperationType& vo )            \
    else                                                                    \
    {                                                                       \
       auto itr = to_tag.find(ar[0].as_string());                           \
-      FC_ASSERT( itr != to_tag.end(), "Invalid operation name: ${n}", ("n", ar[0]) ); \
+      FC_ASSERT( itr != to_tag.end(),                                      \
+         "Invalid operation name: ${n}", ("n", ar[0]) );                   \
       vo.set_which( to_tag[ar[0].as_string()] );                           \
    }                                                                       \
-      vo.visit( fc::to_static_variant( ar[1] ) );                          \
-   }                                                                       \
+   vo.visit( fc::to_static_variant( ar[1] ) );                             \
 }                                                                          \
+} /* namespace fc */                                                       \
                                                                            \
-namespace steemit { namespace protocol {                                      \
+namespace steemit { namespace protocol {                                   \
                                                                            \
 void operation_validate( const OperationType& op )                         \
 {                                                                          \
-   op.visit( steemit::protocol::operation_validate_visitor() );               \
+   op.visit( steemit::protocol::operation_validate_visitor() );            \
 }                                                                          \
                                                                            \
-void operation_get_required_authorities( const OperationType& op,          \
-                                         flat_set< account_name_type >& active,         \
-                                         flat_set< account_name_type >& owner,          \
-                                         flat_set< account_name_type >& posting,        \
-                                         std::vector< authority >& other )     \
+void operation_get_required_authorities(                                   \
+   const OperationType& op,                                                \
+   flat_set< account_name_type >& active,                                  \
+   flat_set< account_name_type >& owner,                                   \
+   flat_set< account_name_type >& posting,                                 \
+   std::vector< authority >& other )                                       \
 {                                                                          \
-   op.visit( steemit::protocol::operation_get_required_auth_visitor( active, owner, posting, other ) ); \
+   op.visit( steemit::protocol::operation_get_required_auth_visitor(       \
+      active, owner, posting, other ) );                                   \
 }                                                                          \
                                                                            \
-} } /* steemit::protocol */
+} } /* namespace steemit::protocol */
