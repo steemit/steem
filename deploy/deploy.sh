22bc9121a9e68422dc21054a6f0e52ecdab4792b
Denis Zakharenkov <zaharden@gmail.com>
2017-04-20 11:24:31 +0700

    Fix.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,8 +4,8 @@ set -xe
 
 if [ $TRAVIS_BRANCH == "master" ]; then
 
-  ./set-ssh.sh
-  ./docs-deploy.sh --yes
+  deploy/set-ssh.sh
+  deploy/docs-deploy.sh --yes
 
 else
   echo "Nothing to deploy, since the current branch is not master."
