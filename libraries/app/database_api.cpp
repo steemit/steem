2b4f091cca79e0373ea8cbbde08fef7832d4d135
Nate Brune <nate@steemit.com>
2017-04-25 15:38:58 -0400

    #801 Adjust filter to not exit early

--- libraries/app/database_api.cpp
+++ libraries/app/database_api.cpp
@@ -1526,7 +1526,7 @@ vector<discussion> database_api::get_discussions_by_cashout( const discussion_qu
       const auto& tidx = my->_db.get_index<tags::tag_index>().indices().get<tags::by_cashout>();
       auto tidx_itr = tidx.lower_bound( boost::make_tuple( tag, fc::time_point::now() - fc::minutes(60) ) );
 
-      return get_discussions( query, tag, parent, tidx, tidx_itr, query.truncate_body, filter_default, []( const comment_api_obj& c ){ return c.net_rshares <= 0; });
+      return get_discussions( query, tag, parent, tidx, tidx_itr, query.truncate_body, []( const comment_api_obj& c ){ return c.net_rshares < 0; });
    });
 }
 
