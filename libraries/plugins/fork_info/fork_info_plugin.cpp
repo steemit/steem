005bcc42812fa96d48963ab197db4d93a4799ca5
Nate Brune <nate@steemit.com>
2017-05-24 20:52:09 -0400

    #963 Initial implementation

---
+++ libraries/plugins/fork_info/fork_info_plugin.cpp
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2015 Cryptonomex, Inc., and contributors.
+ *
+ * The MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include <steemit/fork_info/fork_info_plugin.hpp>
+
+#include <steemit/protocol/types.hpp>
+
+#include <steemit/chain/database.hpp>
+#include <steemit/app/api.hpp>
+#include <steemit/app/database_api.hpp>
+
+#include <fc/network/http/websocket.hpp>
+#include <fc/rpc/websocket_api.hpp>
+#include <fc/api.hpp>
+#include <fc/smart_ref_impl.hpp>
+
+
+namespace steemit { namespace fork_info {
+namespace bpo = boost::program_options;
+
+
+
+namespace detail {
+   using namespace steemit::protocol;
+
+   class fork_info_plugin_impl
+   {
+      public:
+         fork_info_plugin_impl( fork_info_plugin& plugin )
+            :_self( plugin ) {}
+         virtual ~fork_info_plugin_impl() {}
+
+         void on_block( const signed_block& b );
+
+         fork_info_plugin&       _self;
+         std::deque<signed_block> _last_blocks;
+   };
+   void fork_info_plugin_impl::on_block( const signed_block& b )
+   {
+      auto& db = _self.database();
+
+      if( b.block_num() > 21 )
+      {
+         _last_blocks.push_front(b);
+         if(fc::optional<signed_block> block = *db.fetch_block_by_number(_last_blocks.at(22).block_num())){
+            *block.witness_signature != _last_blocks.at(22).witness_signature
+            idump(("Orphaned Block ")(_last_blocks.at(22)));
+         }
+         _last_blocks.pop_back();
+
+      }
+      else
+      {
+         _last_blocks.push_front(b);
+      }
+
+   }
+}
+
+fork_info_plugin::fork_info_plugin( application* app )
+   : plugin( app ), _my( new detail::fork_info_plugin_impl( *this ))
+{}
+
+fork_info_plugin::~fork_info_plugin()
+{}
+
+void fork_info_plugin::plugin_set_program_options(bpo::options_description& cli, bpo::options_description& cfg)
+{}
+
+void fork_info_plugin::plugin_initialize(const boost::program_options::variables_map& options)
+{
+   try
+   {
+      ilog( "fork_info_plugin: plugin_initialize() begin" );
+      chain::database& db = database();
+
+      db.applied_block.connect( [&]( const signed_block& b ){ _my->on_block( b ); } );
+
+      ilog( "fork_info_plugin: plugin_initialize() end" );
+   } FC_CAPTURE_AND_RETHROW()
+}
+
+void fork_info_plugin::plugin_startup()
+{}
+}
+
+STEEMIT_DEFINE_PLUGIN( fork_info, steemit::fork_info::fork_info_plugin )
