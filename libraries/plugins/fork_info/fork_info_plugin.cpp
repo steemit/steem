20a4ce5888d12ba248be89adad82177dda821f43
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2017-06-22 12:34:48 -0400

    Refactor plugin #963

--- libraries/plugins/fork_info/fork_info_plugin.cpp
+++ libraries/plugins/fork_info/fork_info_plugin.cpp
@@ -11,12 +11,12 @@
 #include <fc/api.hpp>
 #include <fc/smart_ref_impl.hpp>
 
-namespace steemit { namespace plugin { namespace fork_info {
+namespace steemit { namespace fork_info {
 
 namespace detail {
    using namespace steemit::protocol;
 
-   class fork_info_plugin_impl
+   struct fork_info_plugin_impl
    {
       public:
          fork_info_plugin_impl( fork_info_plugin& plugin )
@@ -25,63 +25,58 @@ namespace detail {
 
          void on_block( const signed_block& b );
 
-         fork_info_plugin&       _self;
-         std::map<int,std::vector<signed_block>> _last_blocks;
-         std::map<int,std::vector<signed_block>>::iterator it;
+         fork_info_plugin&                                 _self;
+         std::map< uint32_t, std::vector< signed_block > > _last_blocks;
    };
 
    void fork_info_plugin_impl::on_block( const signed_block& b )
    {
       auto& db = _self.database();
-      it = _last_blocks.find(b.block_num());
-      if (it == _last_blocks.end()){
-         std::vector<signed_block> blockv (1, b);
-         _last_blocks.insert( std::pair<int,std::vector<signed_block>>(b.block_num(), blockv) );
-         blockv.clear();
-         if( _last_blocks.size() == 22)
+      auto lib = db.get_dynamic_global_properties().last_irreversible_block_num;
+      
+      if( b.block_num() > lib ) // Should not be needed, but just in case
+         _last_blocks[ b.block_num() ].push_back( b );
+
+      if( _last_blocks.find( lib ) != _last_blocks.end() ) 
+      {
+         auto& blocks = _last_blocks[ lib ];
+         fc::optional< signed_block > included_block = db.fetch_block_by_number( lib );
+         std::set< signature_type > printed_blocks;
+
+         for( auto& orphan : blocks )
          {
-            it = _last_blocks.find(b.block_num()-21);
-            blockv = it->second;
-            fc::optional<signed_block> result = db.fetch_block_by_number(blockv.at(0).block_num());
-            signed_block block = *result;
-            if( result.valid() ){
-               std::vector<signature_type> _printed_blocks;
-               for(unsigned int i=0; i < blockv.size(); i++){
-                  if(block.witness_signature != blockv.at(i).witness_signature){
-                     if(find(_printed_blocks.begin(), _printed_blocks.end(), blockv.at(i).witness_signature) == _printed_blocks.end()){
-                        idump(("Orphaned Block: ")(blockv.at(i))(blockv.at(i).block_num()));
-                        _printed_blocks.push_back(blockv.at(i).witness_signature);
-
-                     }
-                  }
-               }
+            if( orphan.witness_signature != included_block->witness_signature
+               && printed_blocks.find( orphan.witness_signature ) != printed_blocks.end() )
+            {
+               ilog( "Orphaned Block --- Num:${n} Data:${d}", ("n", lib )("d", orphan) );
+               printed_blocks.insert( orphan.witness_signature );  
             }
-            _last_blocks.erase(it);
          }
-      }
-      else {
-         it->second.push_back(b);
-      }
 
+         _last_blocks.erase( lib );
+      }
    }
-}
+} // detail
 
 fork_info_plugin::fork_info_plugin( application* app )
-   : plugin( app ), _my( new detail::fork_info_plugin_impl( *this ))
-{}
+   : plugin( app ), _my( new detail::fork_info_plugin_impl( *this ) ) {}
 
-fork_info_plugin::~fork_info_plugin()
-{}
+fork_info_plugin::~fork_info_plugin() {}
 
-void fork_info_plugin::plugin_initialize(const boost::program_options::variables_map& options)
+void fork_info_plugin::plugin_set_program_options(
+      boost::program_options::options_description& cli,
+      boost::program_options::options_description& cfg ) {}
+
+void fork_info_plugin::plugin_initialize( const boost::program_options::variables_map& options )
 {
    try
    {
-      chain::database& db = database();
-      db.applied_block.connect([this](const chain::signed_block& b){ _my->on_block( b ); });
+      database().applied_block.connect( [&]( const chain::signed_block& b ){ _my->on_block( b ); } );
    } FC_CAPTURE_AND_RETHROW()
 }
 
-void fork_info_plugin::plugin_startup()
-{}
-} } }
+void fork_info_plugin::plugin_startup() {}
+
+} } // steemit::fork_info
+
+STEEMIT_DEFINE_PLUGIN( fork_info, steemit::fork_info::fork_info_plugin )
--- libraries/plugins/fork_info/include/steemit/fork_info/fork_info_plugin.hpp
+++ libraries/plugins/fork_info/include/steemit/fork_info/fork_info_plugin.hpp
@@ -1,25 +1,28 @@
+#pragma once
 #include <steemit/app/plugin.hpp>
 #include <steemit/chain/steem_objects.hpp>
 
-
-namespace steemit { namespace plugin { namespace fork_info {
-namespace detail { struct fork_info_plugin_impl; }
+namespace steemit { namespace fork_info {
 using steemit::app::application;
+
+#define FORK_INFO_PLUGIN_NAME "fork_info"
+
+namespace detail { struct fork_info_plugin_impl; }
+
 class fork_info_plugin : public steemit::app::plugin
 {
    public:
       fork_info_plugin( application* app );
       virtual ~fork_info_plugin();
 
-      std::string plugin_name()const override { return "fork_info"; }
-
+      std::string plugin_name()const override { return FORK_INFO_PLUGIN_NAME; }
+      virtual void plugin_set_program_options(
+         boost::program_options::options_description& cli,
+         boost::program_options::options_description& cfg ) override;
       virtual void plugin_initialize(const boost::program_options::variables_map& options) override;
       virtual void plugin_startup() override;
 
-   private:
-      std::unique_ptr<steemit::plugin::fork_info::detail::fork_info_plugin_impl> _my;
+      std::unique_ptr<steemit::fork_info::detail::fork_info_plugin_impl> _my;
 };
 
-} } } //steemit::plugin::fork_info_info
-
-STEEMIT_DEFINE_PLUGIN( fork_info, steemit::plugin::fork_info::fork_info_plugin )
+} } //steemit::fork_info
