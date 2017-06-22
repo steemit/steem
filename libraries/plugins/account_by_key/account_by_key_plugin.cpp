d7af6facddf76d997a76c33ef1c8b72a38bff5ba
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2017-06-22 11:19:41 -0400

    Added account_create_with_delegation to account_by_key plugin #1194

--- libraries/plugins/account_by_key/account_by_key_plugin.cpp
+++ libraries/plugins/account_by_key/account_by_key_plugin.cpp
@@ -50,6 +50,11 @@ struct pre_operation_visitor
       _plugin.my->clear_cache();
    }
 
+   void operator()( const account_create_with_delegation_operation& op )const
+   {
+      _plugin.my->clear_cache();
+   }
+
    void operator()( const account_update_operation& op )const
    {
       _plugin.my->clear_cache();
@@ -98,6 +103,12 @@ struct post_operation_visitor
    void operator()( const T& )const {}
 
    void operator()( const account_create_operation& op )const
+   {
+      auto acct_itr = _plugin.database().find< account_authority_object, by_account >( op.new_account_name );
+      if( acct_itr ) _plugin.my->update_key_lookup( *acct_itr );
+   }
+
+   void operator()( const account_create_with_delegation_operation& op )const
    {
       auto acct_itr = _plugin.database().find< account_authority_object, by_account >( op.new_account_name );
       if( acct_itr ) _plugin.my->update_key_lookup( *acct_itr );
