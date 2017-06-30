2b1fe5e19ccc3aa413e47f97e7a70537ec625926
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2017-06-30 16:33:53 -0400

    Cleanup

--- libraries/new_plugins/http/http_plugin.cpp
+++ libraries/new_plugins/http/http_plugin.cpp
@@ -3,6 +3,7 @@
 #include <fc/network/ip.hpp>
 #include <fc/log/logger_config.hpp>
 #include <fc/io/json.hpp>
+#include <fc/network/resolve.hpp>
 
 #include <boost/asio.hpp>
 #include <boost/optional.hpp>
@@ -20,7 +21,7 @@
 #include <memory>
 #include <iostream>
 
-#define HTTP_THREAD_POOL_SIZE 256
+#define HTTP_THREAD_POOL_SIZE 64
 
 namespace steemit { namespace plugins { namespace http {
 
@@ -36,7 +37,8 @@ using websocketpp::connection_hdl;
 
 namespace detail {
 
-   struct asio_with_stub_log : public websocketpp::config::asio {
+   struct asio_with_stub_log : public websocketpp::config::asio
+   {
          typedef asio_with_stub_log type;
          typedef asio base;
 
@@ -49,10 +51,6 @@ namespace detail {
          typedef base::con_msg_manager_type con_msg_manager_type;
          typedef base::endpoint_msg_manager_type endpoint_msg_manager_type;
 
-         /// Custom Logging policies
-         //typedef websocketpp::log::syslog<concurrency_type, websocketpp::log::elevel> elog_type;
-         //typedef websocketpp::log::syslog<concurrency_type, websocketpp::log::alevel> alog_type;
-
          //typedef base::alog_type alog_type;
          //typedef base::elog_type elog_type;
          typedef websocketpp::log::stub elog_type;
@@ -60,7 +58,8 @@ namespace detail {
 
          typedef base::rng_type rng_type;
 
-         struct transport_config : public base::transport_config {
+         struct transport_config : public base::transport_config
+         {
             typedef type::concurrency_type concurrency_type;
             typedef type::alog_type alog_type;
             typedef type::elog_type elog_type;
@@ -70,7 +69,7 @@ namespace detail {
                socket_type;
          };
 
-         typedef websocketpp::transport::asio::endpoint<transport_config>
+         typedef websocketpp::transport::asio::endpoint< transport_config >
             transport_type;
 
          static const long timeout_open_handshake = 0;
@@ -78,6 +77,36 @@ namespace detail {
 }
 
 using websocket_server_type = websocketpp::server<detail::asio_with_stub_log>;
+
+std::vector<fc::ip::endpoint> resolve_string_to_ip_endpoints( const std::string& endpoint_string )
+{
+   try
+   {
+      string::size_type colon_pos = endpoint_string.find( ':' );
+      if( colon_pos == std::string::npos )
+         FC_THROW( "Missing required port number in endpoint string \"${endpoint_string}\"",
+                  ("endpoint_string", endpoint_string) );
+
+      std::string port_string = endpoint_string.substr( colon_pos + 1 );
+
+      try
+      {
+         uint16_t port = boost::lexical_cast< uint16_t >( port_string );
+         std::string hostname = endpoint_string.substr( 0, colon_pos );
+         std::vector< fc::ip::endpoint > endpoints = fc::resolve( hostname, port );
+
+         if( endpoints.empty() )
+            FC_THROW_EXCEPTION( fc::unknown_host_exception, "The host name can not be resolved: ${hostname}", ("hostname", hostname) );
+
+         return endpoints;
+      }
+      catch( const boost::bad_lexical_cast& )
+      {
+         FC_THROW("Bad port: ${port}", ("port", port_string) );
+      }
+   }
+   FC_CAPTURE_AND_RETHROW( (endpoint_string) )
+}
 
 class http_plugin_impl
 {
@@ -95,38 +124,46 @@ class http_plugin_impl
       asio::io_service           api_ios;
       asio::io_service::work     api_work;
 
-      //map< string, url_handler > url_handlers;
       optional< tcp::endpoint >  listen_endpoint;
 
       websocket_server_type      server;
+
+      plugins::api_register::api_register_plugin* api;
 };
 
 http_plugin::http_plugin() : appbase::plugin< http_plugin >( HTTP_PLUGIN_NAME ), _my( new http_plugin_impl() ) {}
 http_plugin::~http_plugin(){}
 
-void http_plugin::set_program_options(options_description&, options_description& cfg) {
+void http_plugin::set_program_options( options_description&, options_description& cfg )
+{
    cfg.add_options()
-         ("http-server-endpoint", bpo::value<string>()->default_value("127.0.0.1:8888"),
-            "The local IP and port to listen for incoming http connections.")
+         ("http-server-endpoint", bpo::value<string>()->default_value("127.0.0.1:8888"), "The local IP and port to listen for incoming http connections.")
          ;
+   //cli.add( cfg );
 }
 
-void http_plugin::plugin_initialize(const variables_map& options) {
-   if(options.count("http-server-endpoint")) {
-      auto lipstr = options.at("http-server-endpoint").as< string >();
-      auto fcep = fc::ip::endpoint::from_string(lipstr);
-      _my->listen_endpoint = tcp::endpoint(boost::asio::ip::address_v4::from_string((string)fcep.get_address()), fcep.port());
-      ilog("configured http to listen on ${ep}", ("ep", fcep));
+void http_plugin::plugin_initialize( const variables_map& options )
+{
+   if( options.count( "http-server-endpoint" ) )
+   {
+      auto http_endpoint = options.at( "http-server-endpoint" ).as< string >();
+      auto endpoints = resolve_string_to_ip_endpoints( http_endpoint );
+      FC_ASSERT( endpoints.size(), "http-server-endpoint ${hostname} did not resolve", ("hostname", http_endpoint) );
+      _my->listen_endpoint = tcp::endpoint( boost::asio::ip::address_v4::from_string( ( string )endpoints[0].get_address() ), endpoints[0].port() );
+      ilog( "configured http to listen on ${ep}", ("ep", endpoints[0]) );
    }
 }
 
 void http_plugin::plugin_startup()
 {
-   if(_my->listen_endpoint)
+   if( _my->listen_endpoint )
    {
-      _my->http_thread = std::make_shared<std::thread>([&]()
+      _my->api = appbase::app().find_plugin< plugins::api_register::api_register_plugin >();
+      FC_ASSERT( _my->api != nullptr, "Could not find API Register Plugin" );
+
+      _my->http_thread = std::make_shared<std::thread>( [&]()
       {
-         ilog("start processing http thread");
+         ilog( "start processing http thread" );
          try
          {
             _my->server.clear_access_channels( websocketpp::log::alevel::all );
@@ -134,73 +171,57 @@ void http_plugin::plugin_startup()
             _my->server.init_asio( &_my->http_ios );
             _my->server.set_reuse_addr( true );
 
-            _my->server.set_http_handler([&](connection_hdl hdl)
+            _my->server.set_http_handler( [&]( connection_hdl hdl )
             {
-               auto con = _my->server.get_con_from_hdl(hdl);
+               auto con = _my->server.get_con_from_hdl( hdl );
                con->defer_http_response();
 
-               _my->api_ios.post( [con]()
+               _my->api_ios.post( [con, this]()
                {
                   auto body = con->get_request_body();
-                  auto resource = con->get_uri()->get_resource();
 
-                  con->set_body( appbase::app().get_plugin< plugins::api_register::api_register_plugin >().call_api( body ) );
-                  con->set_status( websocketpp::http::status_code::ok );
+                  try
+                  {
+                     con->set_body( _my->api->call_api( body ) );
+                     con->set_status( websocketpp::http::status_code::ok );
+                  }
+                  catch( fc::exception& e )
+                  {
+                     con->set_body( "Could not call API" );
+                     con->set_status( websocketpp::http::status_code::not_found );
+                  }
+
                   con->send_http_response();
                });
-
-               //ilog("handle http request: ${url}", ("url",con->get_uri()->str()));
-               //ilog("${body}", ("body", con->get_request_body()));
-               //ilog("${resource}", ("resource", con->get_uri()->get_resource()) );
-
-               //auto body = con->get_request_body();
-               //auto resource = con->get_uri()->get_resource();
-               //auto handler_itr = _my->url_handlers.find(resource);
-               //idump( (body) );
-               //auto json_var = fc::json::from_string( body );
-               //idump( (json_var) );
-
-               //con->set_body( body );
-               //con->set_status(websocketpp::http::status_code::value(200));
-               /*if(handler_itr != _my->url_handlers.end()) {
-                  handler_itr->second(resource, body, [con,this](int code, string body) {
-                     con->set_body(body);
-                     con->set_status(websocketpp::http::status_code::value(code));
-                  });
-               } else {
-                  wlog("404 - not found: ${ep}", ("ep",resource));
-                  con->set_body("Unknown Endpoint");
-                  con->set_status(websocketpp::http::status_code::not_found);
-               }*/
             });
 
-            ilog("start listening for http requests");
-            _my->server.listen(*_my->listen_endpoint);
+            ilog( "start listening for http requests" );
+            _my->server.listen( *_my->listen_endpoint );
             _my->server.start_accept();
 
             _my->http_ios.run();
-            ilog("http io service exit");
-         } catch (...) {
-               elog("error thrown from http io service");
+            ilog( "http io service exit" );
+         }
+         catch( ... )
+         {
+            elog( "error thrown from http io service" );
          }
       });
 
    }
 }
 
-void http_plugin::plugin_shutdown() {
-   if(_my->http_thread) {
-      if(_my->server.is_listening())
-            _my->server.stop_listening();
+void http_plugin::plugin_shutdown()
+{
+   if( _my->http_thread )
+   {
+      if( _my->server.is_listening() )
+         _my->server.stop_listening();
+
       _my->http_ios.stop();
       _my->http_thread->join();
       _my->http_thread.reset();
    }
 }
 
-/*void http_plugin::add_handler(const string& url, const url_handler& handler) {
-   _my->http_ios.post([=](){
-      _my->url_handlers.insert(std::make_pair(url,handler));
-   });
-}*/
 } } } // steemit::plugins::http
--- libraries/new_plugins/http/include/steemit/plugins/http/http_plugin.hpp
+++ libraries/new_plugins/http/include/steemit/plugins/http/http_plugin.hpp
@@ -13,39 +13,9 @@ namespace steemit { namespace plugins { namespace http {
 using namespace appbase;
 
 /**
-  * @brief A callback function provided to a URL handler to
-  * allow it to specify the HTTP response code and body
-  *
-  * Arguments: response_code, response_body
-  */
-//using url_response_callback = std::function< void(int, string) >;
-
-/**
-  * @brief Callback type for a URL handler
-  *
-  * URL handlers have this type
-  *
-  * The handler must gaurantee that url_response_callback() is called;
-  * otherwise, the connection will hang and result in a memory leak.
-  *
-  * Arguments: url, request_body, response_callback
-  */
-//using url_handler = std::function< void(string, string, url_response_callback) >;
-
-/**
-  * @brief An API, containing URLs and handlers
-  *
-  * An API is composed of several calls, where each call has a URL and
-  * a handler. The URL is the path on the web server that triggers the
-  * call, and the handler is the function which implements the API call
-  */
-//using api_description = std::map<string, url_handler>;
-
-/**
   * This plugin starts an HTTP server and dispatches queries to
-  * registered handles based upon URL. The handler is passed the
-  * URL that was requested and a callback method that should be
-  * called with the response code and body.
+  * registered handles based on payload. The payload must be conform
+  * to the JSONRPC 2.0 spec.
   *
   * The handler will be called from the appbase application io_service
   * thread.  The callback can be called from any thread and will
@@ -67,12 +37,6 @@ class http_plugin : public appbase::plugin< http_plugin >
       void plugin_initialize(const variables_map& options);
       void plugin_startup();
       void plugin_shutdown();
-
-      /*void add_handler(const string& url, const url_handler&);
-      void add_api(const api_description& api) {
-         for (const auto& call : api)
-            add_handler(call.first, call.second);
-      }*/
 
    private:
       std::unique_ptr< class http_plugin_impl > _my;
