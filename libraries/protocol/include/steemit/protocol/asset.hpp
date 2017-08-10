84fc8cfcda8ae62d5fd87c4a549af8107f3c0073
Bartek Wrona <vogel76@users.noreply.github.com>
2017-08-10 17:58:17 +0200

    Issue 991 final changes (#1350)
    * Issue 991 final changes
    
    1) price default constructor is allowed again, because of fc::variant::as method needs. 2) price(const asset&, const asset&) constructor is declared as explicit, and passed arguments must be filled. 
    3) Added asset operator * ( const asset& a, const asset& b ), needed after blocking implicit convertion to price (previously asset operator *  ( const asset& a, const price& b ); was chosen).
    
    * 991-explicit price-constructor fixes to pass chain_tests
    
    - eliminated one of previously added assertions, be too strict, even passed data can lead to creation of invalid price object.
    - updated comments
    
    * Updated submodule libraries/fc
    
    * Required changes related to price validation 
    
    price explicit constructor can throw an exception now when passed args are broken
    Extended operation_tests by case when validation is done directly inside explicit price constructor
    
    * Revert "Updated submodule libraries/fc"
    
    This reverts commit 6b0f8f80bbcfc725e4dd06dcf64bae18654da8fe.

--- libraries/protocol/include/steemit/protocol/asset.hpp
+++ libraries/protocol/include/steemit/protocol/asset.hpp
@@ -78,13 +78,46 @@ namespace steemit { namespace protocol {
          FC_ASSERT( a.symbol == b.symbol );
          return asset( a.amount + b.amount, a.symbol );
       }
-
+      
+      friend asset operator * ( const asset& a, const asset& b )
+      {
+         FC_ASSERT( a.symbol == b.symbol );
+         return asset( a.amount * b.amount, a.symbol );
+      }
    };
 
+   /** Represents quotation of the relative value of asset against another asset.
+       Similar to 'currency pair' used to determine value of currencies.
+
+       For example:
+       1 EUR / 1.25 USD where:
+       1 EUR is an asset specified as a base
+       1.25 USD us an asset specified as a qute
+
+       can determine value of EUR against USD.
+   */
    struct price
    {
-      price(const asset& base = asset(), const asset& quote = asset())
-         : base(base),quote(quote){}
+      /** Even non-single argument, lets make it an explicit one to avoid implicit calls for 
+          initialization lists. 
+
+          \param base  - represents a value of the price object to be expressed relatively to quote
+                         asset. Cannot have amount == 0 if you want to build valid price.
+          \param quote - represents an relative asset. Cannot have amount == 0, otherwise
+                         asertion fail.
+
+        Both base and quote shall have different symbol defined, since it also results in 
+        creation of invalid price object. \see validate() method.
+      */
+      explicit price(const asset& base, const asset& quote) : base(base),quote(quote)
+      {
+          /// Call validate to verify passed arguments. \warning It throws on error.
+          validate();
+      }
+
+      /** Default constructor is needed because of fc::variant::as method requirements.
+      */
+      price() = default;
 
       asset base;
       asset quote;
@@ -99,7 +132,7 @@ namespace steemit { namespace protocol {
 
       bool is_null()const;
       void validate()const;
-   };
+   }; /// price
 
    price operator / ( const asset& base, const asset& quote );
    inline price operator~( const price& p ) { return price{p.quote,p.base}; }
