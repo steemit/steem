ca24741636e2cd83ec22898fc88d177b7c3b4656
theoreticalbts <theoreticalbts@users.noreply.github.com>
2017-08-29 15:35:42 -0400

    Step 1:  Define smt_elevate_account_operation structure #1457

--- libraries/protocol/include/steemit/protocol/smt_operations.hpp
+++ libraries/protocol/include/steemit/protocol/smt_operations.hpp
@@ -5,6 +5,21 @@
 
 namespace steemit { namespace protocol {
 
+/**
+ * This operation elevates given steem account to SMT control account on conditions that:
+ * a) the account is not SMT control account already and
+ * b) successful fee transfer occurs.
+ */
+struct smt_elevate_account_operation : public base_operation
+{
+   /// Account to be elevated.
+   account_name_type account;
+   /// The amount to be transfered from @account to null account as elevation fee.
+   asset             fee;
+
+   extensions_type   extensions;
+};
+
 // TODO:  Rename these curves to match naming in manual.md
 // TODO:  Remove duplicate definition in steemit::chain
 enum curve_id
