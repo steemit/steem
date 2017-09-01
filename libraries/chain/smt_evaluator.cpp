dbb98b7daee74983029d1c99b572da72abad8512
Łukasz Bujak <lbujak@steemit.com>
2017-09-01 17:01:27 +0200

    Added assertion checking symbol of dgpo.smt_creation_fee. #1457

--- libraries/chain/smt_evaluator.cpp
+++ libraries/chain/smt_evaluator.cpp
@@ -17,13 +17,15 @@ void smt_elevate_account_evaluator::do_apply( const smt_elevate_account_operatio
    FC_ASSERT( _db.has_hardfork( STEEM_SMT_HARDFORK ), "SMT functionality not enabled until hardfork ${hf}", ("hf", STEEM_SMT_HARDFORK) );
    const dynamic_global_property_object& dgpo = _db.get_dynamic_global_properties();
 
-   asset effective_creation_fee;
+   asset effective_elevation_fee;
 
+   FC_ASSERT( dgpo.smt_creation_fee.symbol == STEEM_SYMBOL || dgpo.smt_creation_fee.symbol == SBD_SYMBOL,
+      "Unexpected internal error - wrong symbol ${s} of account elevation fee.", ("s", dgpo.smt_creation_fee.symbol) );
    FC_ASSERT( o.fee.symbol == STEEM_SYMBOL || o.fee.symbol == SBD_SYMBOL,
-              "Asset fee must be STEEM or SBD, was ${s}", ("s", o.fee.symbol) );
+      "Asset fee must be STEEM or SBD, was ${s}", ("s", o.fee.symbol) );
    if( o.fee.symbol == dgpo.smt_creation_fee.symbol )
    {
-      effective_creation_fee = dgpo.smt_creation_fee;
+      effective_elevation_fee = dgpo.smt_creation_fee;
    }
    else
    {
@@ -31,16 +33,16 @@ void smt_elevate_account_evaluator::do_apply( const smt_elevate_account_operatio
       FC_ASSERT( !fhistory.current_median_history.is_null(), "Cannot pay the fee using SBD because there is no price feed." );
       if( o.fee.symbol == STEEM_SYMBOL )
       {
-         effective_creation_fee = _db.to_sbd( o.fee );
+         effective_elevation_fee = _db.to_sbd( o.fee );
       }
       else
       {
-         effective_creation_fee = _db.to_steem( o.fee );         
+         effective_elevation_fee = _db.to_steem( o.fee );         
       }
    }
 
    const account_object& acct = _db.get_account( o.account );
-   FC_ASSERT( o.fee >= effective_creation_fee, "Fee of ${of} is too small, must be at least ${ef}", ("of", o.fee)("ef", effective_creation_fee) );
+   FC_ASSERT( o.fee >= effective_elevation_fee, "Fee of ${of} is too small, must be at least ${ef}", ("of", o.fee)("ef", effective_elevation_fee) );
    FC_ASSERT( _db.get_balance( acct, o.fee.symbol ) >= o.fee, "Account does not have sufficient funds for specified fee of ${of}", ("of", o.fee) );
 
    const account_object& null_account = _db.get_account( STEEM_NULL_ACCOUNT );
