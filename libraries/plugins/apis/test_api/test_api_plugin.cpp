5f028c6cf0c17612f12c304fc79c1a645d280624
theoreticalbts <theoreticalbts@users.noreply.github.com>
2017-09-12 16:58:08 -0400

    Fix test_api_plugin build #1384

--- libraries/plugins/apis/test_api/test_api_plugin.cpp
+++ libraries/plugins/apis/test_api/test_api_plugin.cpp
@@ -15,4 +15,18 @@ void test_api_plugin::plugin_initialize( const variables_map& options )
 void test_api_plugin::plugin_startup() {}
 void test_api_plugin::plugin_shutdown() {}
 
+test_api_a_return test_api_plugin::test_api_a( const test_api_a_args& args )
+{
+   test_api_a_return result;
+   result.value = "A";
+   return result;
+}
+
+test_api_b_return test_api_plugin::test_api_b( const test_api_b_args& args )
+{
+   test_api_b_return result;
+   result.value = "B";
+   return result;
+}
+
 } } } // steem::plugins::test_api
