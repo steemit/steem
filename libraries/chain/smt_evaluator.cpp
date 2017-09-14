c69ffdcebe23f039af5b6c8a50be43e87a165a06
Marek Kochanowicz <mkochanowicz@steemit.com>
2017-09-14 14:23:40 +0200

    implement `smt_set_setup_parameters_evaluator::do_apply` method - fix #1516

--- libraries/chain/smt_evaluator.cpp
+++ libraries/chain/smt_evaluator.cpp
@@ -12,6 +12,31 @@
 #ifdef STEEM_ENABLE_SMT
 namespace steem { namespace chain {
 
+namespace {
+
+class smt_setup_parameters_visitor : public fc::visitor<bool>
+{
+public:
+   smt_setup_parameters_visitor(smt_token_object& smt_token) : _smt_token(smt_token) {}
+
+   bool operator () (const smt_param_allow_voting& allow_voting)
+   {
+      _smt_token.allow_voting = allow_voting.value;
+      return true;
+   }
+
+   bool operator () (const smt_param_allow_vesting& allow_vesting)
+   {
+      _smt_token.allow_vesting = allow_vesting.value;
+      return true;
+   }
+
+private:
+   smt_token_object& _smt_token;
+};
+
+} // namespace
+
 void smt_elevate_account_evaluator::do_apply( const smt_elevate_account_operation& o )
 {
    FC_ASSERT( _db.has_hardfork( STEEM_SMT_HARDFORK ), "SMT functionality not enabled until hardfork ${hf}", ("hf", STEEM_SMT_HARDFORK) );
@@ -79,6 +104,19 @@ void smt_setup_inflation_evaluator::do_apply( const smt_setup_inflation_operatio
 void smt_set_setup_parameters_evaluator::do_apply( const smt_set_setup_parameters_operation& o )
 {
    FC_ASSERT( _db.has_hardfork( STEEM_SMT_HARDFORK ), "SMT functionality not enabled until hardfork ${hf}", ("hf", STEEM_SMT_HARDFORK) );
+   auto& smt_token_by_account = _db.get_index<smt_token_index>().indices().get<by_control_account>();
+   auto it = smt_token_by_account.find(o.control_account);
+   FC_ASSERT( it != smt_token_by_account.end(), "SMT ${ac} not elevated yet.", ("ac", o.control_account));
+   const smt_token_object& smt_token = *it;
+   FC_ASSERT( smt_token.phase == smt_token_object::smt_phase::account_elevated, "SMT ${ac} setup already completed.", ("ac", o.control_account));
+   
+   _db.modify( smt_token, [&]( smt_token_object& token )
+   {
+      smt_setup_parameters_visitor visitor(token);
+
+      for (auto& param : o.setup_parameters)
+         param.visit(visitor);
+   });
 }
 
 void smt_set_runtime_parameters_evaluator::do_apply( const smt_set_runtime_parameters_operation& o )
