dda436d8321fc71c44ab88e09e706d3487fc9af4
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2017-10-23 15:03:01 -0700

    Fix history pruning cli parsing #1671

--- libraries/plugins/account_history/account_history_plugin.cpp
+++ libraries/plugins/account_history/account_history_plugin.cpp
@@ -280,7 +280,7 @@ void account_history_plugin::plugin_initialize(const boost::program_options::var
 
    if( options.count( "history-disable-pruning" ) )
    {
-      my->_prune = options[ "history-disable-pruning" ].as< bool >();
+      my->_prune = !options[ "history-disable-pruning" ].as< bool >();
    }
 }
 
