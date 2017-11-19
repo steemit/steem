ee01e8d40aaeb3d72424b55a7a65e6f164cebbfe
Bartek Wrona <wrona@syncad.com>
2017-11-19 23:57:54 +0100

    Further ra_indexed_container development
    1) Fixed bug in item erase - RA-index of deleted item hasn't been stored for reuse, what lead next to conflict with insertion of another object.
    2) Implemented ra_indexed_container operation dump to simplify debugging.
    3) Fixed crash in tags_plugin, where current tag was deleted during iteration what lead to random crash.

--- libraries/chain/steem_objects.cpp
+++ libraries/chain/steem_objects.cpp
@@ -3,6 +3,24 @@
 
 #include <fc/uint128.hpp>
 
+#include <fc/variant.hpp>
+#include <fc/reflect/variant.hpp>
+#include <fc/exception/exception.hpp>
+#include <fc/io/json.hpp>
 namespace steem { namespace chain {
 
 } } // steem::chain
+
+
+namespace chainbase
+{
+namespace helpers
+{
+
+   std::string ra_index_container_logging_dumper<steem::chain::comment_object>::dump(const steem::chain::comment_object& o)
+   {
+      auto jsonDump = fc::json::to_pretty_string(o);
+      return jsonDump;
+   }
+}
+}
