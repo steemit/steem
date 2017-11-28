91ac63c0f7c87f150c8b5c811dc34911a582bbb4
Bartek Wrona <wrona@syncad.com>
2017-11-28 17:06:21 +0100

    Unified account_balance_index definition, updated comments.

--- libraries/chain/include/steem/chain/smt_objects/account_balance_object.hpp
+++ libraries/chain/include/steem/chain/smt_objects/account_balance_object.hpp
@@ -7,13 +7,19 @@
 
 namespace steem { namespace chain {
 
+/**
+ * Class responsible for holding non STEEM balance (SMT one) for given account.
+ * Since multiple kinds of balances are possible (atm regular and savings) this
+ * class is parameterized to distinguish their types. 
+ */
 template <enum object_type ObjectType>
 class account_balance_object : public object< ObjectType, account_balance_object<ObjectType> >
 {
    account_balance_object() = delete;
 
-public:
+public:   
    typedef account_balance_object<ObjectType>   _this_type;
+   using typename object< ObjectType, _this_type >::id_type;
 
    template <class Constructor, class Allocator>
    account_balance_object(Constructor&&, allocator< Allocator >)
@@ -22,15 +28,18 @@ public:
    }
 
    // id_type is actually oid<account_balance_object<ObjectType> >
-   typename object< ObjectType, _this_type >::id_type id;
-   account_name_type                                  owner = "@@@@@";
-   asset                                              balance;
+   id_type             id;
+   /// Name of the account, the balance is held for.
+   account_name_type   owner = "@@@@@";
+   asset               balance;
 
+   /// Returns true if this object holds a savings balance, regular one otherwise.
    bool is_savings_balance() const
    {
       return ObjectType == account_savings_balance_object_type;
    }
 
+   /// Returns symbol of the asset the balance is held for.
    asset_symbol_type get_symbol() const
    {
       return balance.symbol;
@@ -39,37 +48,27 @@ public:
 
 struct by_owned_symbol;
 
-typedef multi_index_container <
-   account_regular_balance_object,
+template <enum object_type ObjectType>
+using account_balance_index = chainbase::shared_multi_index_container <account_balance_object<ObjectType>,
    indexed_by <
       ordered_unique< tag< by_id >,
-         member< account_regular_balance_object, account_regular_balance_id_type, &account_regular_balance_object::id > >,
-      ordered_unique< tag< by_owned_symbol >,
-         composite_key< account_regular_balance_object,
-            member< account_regular_balance_object, account_name_type, &account_regular_balance_object::owner >,
-            const_mem_fun< account_regular_balance_object, asset_symbol_type, &account_regular_balance_object::get_symbol >
-         >,
-         composite_key_compare< std::less< account_name_type >, std::less< asset_symbol_type > >
+         member< account_balance_object<ObjectType>, typename account_balance_object<ObjectType>::id_type, &account_balance_object<ObjectType>::id>
+      >,
+      ordered_unique< tag< by_account >,
+         member< account_balance_object<ObjectType>, account_name_type, &account_balance_object<ObjectType>::owner>
+      >,
+      ordered_unique<tag<by_owned_symbol>,
+         composite_key<account_balance_object<ObjectType>,
+            member<account_balance_object<ObjectType>, typename account_balance_object<ObjectType>::id_type, &account_balance_object<ObjectType>::id>,
+            const_mem_fun< account_balance_object<ObjectType>, asset_symbol_type, &account_balance_object<ObjectType>::get_symbol >
+         >
       >
-   >,
-   allocator< account_regular_balance_object >
-> account_regular_balance_index;
+   >
+>;
 
-typedef multi_index_container <
-   account_savings_balance_object,
-   indexed_by <
-      ordered_unique< tag< by_id >,
-         member< account_savings_balance_object, account_savings_balance_id_type, &account_savings_balance_object::id > >,
-      ordered_unique< tag< by_owned_symbol >,
-         composite_key< account_savings_balance_object,
-            member< account_savings_balance_object, account_name_type, &account_savings_balance_object::owner >,
-            const_mem_fun< account_savings_balance_object, asset_symbol_type, &account_savings_balance_object::get_symbol >
-         >,
-         composite_key_compare< std::less< account_name_type >, std::less< asset_symbol_type > >
-      >
-   >,
-   allocator< account_savings_balance_object >
-> account_savings_balance_index;
+typedef account_balance_index<account_regular_balance_object_type> account_regular_balance_index;
+typedef account_balance_index<account_savings_balance_object_type> account_savings_balance_index;
+
 
 } } // namespace steem::chain
 
