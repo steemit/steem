00dc70174d6f4f3dfdd1955a82387ee2a6844657
theoreticalbts <theoreticalbts@users.noreply.github.com>
2017-12-13 15:59:09 -0500

    A few more fixes to rename schema #1897

--- libraries/chain/include/steem/chain/custom_operation_interpreter.hpp
+++ libraries/chain/include/steem/chain/custom_operation_interpreter.hpp
@@ -3,7 +3,7 @@
 
 #include <memory>
 
-namespace graphene { namespace schema {
+namespace steem { namespace schema {
    struct abstract_schema;
 } }
 
@@ -18,7 +18,7 @@ class custom_operation_interpreter
    public:
       virtual void apply( const protocol::custom_json_operation& op ) = 0;
       virtual void apply( const protocol::custom_binary_operation & op ) = 0;
-      virtual std::shared_ptr< graphene::schema::abstract_schema > get_operation_schema() = 0;
+      virtual std::shared_ptr< steem::schema::abstract_schema > get_operation_schema() = 0;
 };
 
 } } // steem::chain
