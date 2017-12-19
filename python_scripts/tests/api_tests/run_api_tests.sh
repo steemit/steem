d41754789bf80722fcf35f0522bc9ec7d50a6f1b
Łukasz Bujak <eu-be@tlen.pl>
2017-12-19 16:46:11 +0100

    First tests for account history API #1567

--- python_scripts/tests/api_tests/run_api_tests.sh
+++ python_scripts/tests/api_tests/run_api_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-RPC_PORT=9876
+NODE='http://127.0.0.1'
+RPC_PORT=8090
 EXIT_CODE=0
 COMPARATOR=''
 
@@ -16,10 +17,13 @@ fi
 
 echo COMPARATOR: $COMPARATOR
 
-pyresttest http://127.0.0.1:$RPC_PORT ./basic_smoketest.yaml
+pyresttest $NODE:$RPC_PORT ./basic_smoketest.yaml
 [ $? -ne 0 ] && echo FATAL: steemd not running? && exit -1
 
-pyresttest http://127.0.0.1:$RPC_PORT ./database_api/database_api_test.yaml --import_extensions='validator_ex;'$COMPARATOR
+pyresttest $NODE:$RPC_PORT ./database_api/database_api_test.yaml --import_extensions='validator_ex;'$COMPARATOR
+[ $? -ne 0 ] && EXIT_CODE=-1
+
+pyresttest $NODE:$RPC_PORT ./account_history_api/account_history_api_test.yaml --import_extensions='validator_ex;'$COMPARATOR
 [ $? -ne 0 ] && EXIT_CODE=-1
 
 exit $EXIT_CODE
