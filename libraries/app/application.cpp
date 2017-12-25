e38dd8cd6b81cddccaf53fe29aaf39d86ab9f5de
AKorpusenko <antokorp@gmail.com>
2017-12-25 18:33:32 +0300

    Fixed a small error to make golosd possible to be build.

--- libraries/app/application.cpp
+++ libraries/app/application.cpp
@@ -158,9 +158,10 @@ namespace steemit {
                             return;
                         }
 
-                        auto rate_limit_per_second = _options->at("rate-limit-per-second").as<uint64_t>();
+                        // auto rate_limit_per_second = _options->at("rate-limit-per-second").as<uint64_t>();
 
-                        _websocket_server = std::make_shared<fc::http::websocket_server>(rate_limit_per_second);
+                        // _websocket_server = std::make_shared<fc::http::websocket_server>(rate_limit_per_second);
+                        _websocket_server = std::make_shared<fc::http::websocket_server>();
 
                         _websocket_server->on_connection([&](const fc::http::websocket_connection_ptr &c) { on_connection(c); });
                         auto rpc_endpoint = _options->at("rpc-endpoint").as<string>();
