6885e52839182c898cb2c8837aeff037d3c1d41f
Bartek Wrona <wrona@syncad.com>
2017-12-30 14:01:48 +0100

    Underlying RocksDB created and initialized successfully.

--- libraries/plugins/rocksdb/rocksdb_plugin.cpp
+++ libraries/plugins/rocksdb/rocksdb_plugin.cpp
@@ -5,6 +5,14 @@
 
 #include <steem/utilities/benchmark_dumper.hpp>
 
+#include <appbase/application.hpp>
+
+#include <string>
+
+#include "rocksdb/db.h"
+#include "rocksdb/slice.h"
+#include "rocksdb/options.h"
+
 namespace bpo = boost::program_options;
 
 namespace steem { namespace plugins { namespace rocksdb {
@@ -16,15 +24,43 @@ using steem::protocol::signed_transaction;
 
 using steem::utilities::benchmark_dumper;
 
+using ::rocksdb::DB;
+using ::rocksdb::Options;
+
 class rocksdb_plugin::impl
 {
 public:
    impl() : _mainDb(appbase::app().get_plugin<steem::plugins::chain::chain_plugin>().db()) {}
    
-   void openDb(const std::string& path)
+   void openDb(const bfs::path& path)
    {
+      DB* storageDb = nullptr;
+      Options options;
+      // Optimize RocksDB. This is the easiest way to get RocksDB to perform well
+      options.IncreaseParallelism();
+      options.OptimizeLevelStyleCompaction();
+      // create the DB if it's not already present
+      options.create_if_missing = true;
+      
+      auto strPath = path.string();
+      auto status = DB::Open(options, strPath, &storageDb);
+
+      if(status.ok())
+      {
+         ilog("RocksDB opened successfully");
+      }
+      else
+      {
+         elog("RocksDB cannot open database at location: `${p}'.\nReturned error: ${e}",
+            ("p", strPath)("e", status.ToString()));
+      }
+
+      _storage.reset(storageDb);
+
+      importData();
    }
 
+private:
    void importData()
    {
       ilog("Starting data import...");
@@ -73,7 +109,8 @@ public:
    }
 
 private:
-   const chain::database& _mainDb;
+   const chain::database&         _mainDb;
+   std::unique_ptr<::rocksdb::DB> _storage;
 };
 
 rocksdb_plugin::rocksdb_plugin()
@@ -90,22 +127,31 @@ void rocksdb_plugin::set_program_options(
    boost::program_options::options_description &config_file_options)
 {
    command_line_options.add_options()
-      ("rocksdb-path", bpo::value<string>(), "Allows to specify path where rocksdb store will be located.")
+      ("rocksdb-path", bpo::value<bfs::path>()->default_value("rocksdb_storage"),
+         "Allows to specify path where rocksdb store will be located.")
    ;
 }
 
 void rocksdb_plugin::plugin_initialize(const boost::program_options::variables_map& options)
 {
    if(options.count("rocksdb-path"))
-      _dbPath = options.at("rocksdb-path").as<string>();
+      _dbPath = options.at("rocksdb-path").as<bfs::path>();
 }
 
 void rocksdb_plugin::plugin_startup()
 {
    ilog("Starting up rocksdb_plugin...");
 
-   my = std::make_unique<impl>();
-   my->importData();
+   _my = std::make_unique<impl>();
+   if(_dbPath.is_absolute())
+   {
+      _my->openDb(_dbPath);
+   }
+   else
+   {
+      auto actualPath = appbase::app().data_dir() / _dbPath;
+      _my->openDb(actualPath);
+   }
 }
 
 void rocksdb_plugin::plugin_shutdown()
