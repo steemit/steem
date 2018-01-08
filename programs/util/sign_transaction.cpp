2553cc32fe6415b97fad1c3b7e5e52c1510f6307
Bartek Wrona <wrona@syncad.com>
2018-01-08 22:20:12 +0100

    Issue #1751 Improved error messaging, eliminated crashes found while testing, added example inputs.

--- programs/util/sign_transaction.cpp
+++ programs/util/sign_transaction.cpp
@@ -84,9 +84,18 @@ int main(int argc, char** argv, char** envp)
 
       if(*strChainId != '\0')
       {
-         std::cerr << "Using explicit chain-id: `" << strChainId << "'" << std::endl;
-         fc::sha256 parsedId(strChainId);
-         chainId = parsedId;
+         try
+         {
+            fc::sha256 parsedId(strChainId);
+            chainId = parsedId;
+            std::cerr << "Using explicit chain-id: `" << strChainId << "'" << std::endl;
+         }
+         catch(const fc::exception& e)
+         {
+            std::cerr << "Specified explicit chain-id : `" << strChainId << "' is invalid. Option ignored, default chain-id used." << std::endl;
+            auto error = e.to_detail_string();
+            std::cerr << error << std::endl;
+         }
       }
    }
 
@@ -105,16 +114,28 @@ int main(int argc, char** argv, char** envp)
          fc::variant v = fc::json::from_string( line, fc::json::strict_parser );
          tx_signing_request sreq;
          fc::from_variant( v, sreq );
+
          tx_signing_result sres;
          sres.tx = sreq.tx;
          sres.digest = sreq.tx.digest();
          sres.sig_digest = sreq.tx.sig_digest(chainId);
 
-         fc::ecc::private_key priv_key = *steem::utilities::wif_to_key( sreq.wif );
-         sres.sig = priv_key.sign_compact( sres.sig_digest );
-         sres.key = steem::protocol::public_key_type( priv_key.get_public_key() );
-         std::string sres_str = fc::json::to_string( sres );
-         std::cout << "{\"result\":" << sres_str << "}" << std::endl;
+         auto priv_key = steem::utilities::wif_to_key( sreq.wif );
+
+         if(priv_key)
+         {
+            sres.sig = priv_key->sign_compact( sres.sig_digest );
+            sres.key = steem::protocol::public_key_type( priv_key->get_public_key() );
+            std::string sres_str = fc::json::to_string( sres );
+            std::cout << "{\"result\":" << sres_str << "}" << std::endl;
+         }
+         else
+         {
+            if(sreq.wif.empty())
+               std::cerr << "Missing Wallet Import Format in the input JSON structure" << std::endl;
+            else
+               std::cerr << "Passed JSON points to invalid data according to Wallet Import Format specification: `" << sreq.wif << "'" << std::endl;
+         }
       }
       catch( const fc::exception& e )
       {
