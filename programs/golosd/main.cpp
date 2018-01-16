65c831f808d42672d79e1c25ed17f33f79025586
Vladimir Pavliv <vovchapavliv@gmail.com>
2018-01-16 17:12:54 +0300

    Account by key plugin implementation

--- programs/golosd/main.cpp
+++ programs/golosd/main.cpp
@@ -13,8 +13,7 @@
 #include <golos/plugins/test_api/test_api_plugin.hpp>
 #include <golos/plugins/tolstoy_api/tolstoy_api_plugin.hpp>
 #include <golos/plugins/blockchain_statistics/plugin.hpp>
-
-
+#include <golos/plugins/account_by_key/account_by_key_plugin.hpp>
 
 #include <fc/interprocess/signals.hpp>
 #include <fc/log/console_appender.hpp>
@@ -92,6 +91,7 @@ int main( int argc, char** argv ) {
         appbase::app().register_plugin<golos::plugins::test_api::test_api_plugin>();
         appbase::app().register_plugin<golos::plugins::tolstoy_api::tolstoy_api_plugin>();
         appbase::app().register_plugin<golos::plugins::blockchain_statistics::plugin>();
+        appbase::app().register_plugin<golos::plugins::account_by_key::account_by_key_plugin>();
 ///plugins
         appbase::app().set_version_string( version_string() );
 
