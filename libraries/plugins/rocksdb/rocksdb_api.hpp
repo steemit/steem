0d74a7fca1c6171b2541f4542a55fceeeb82abe9
Bartek Wrona <wrona@syncad.com>
2018-01-19 19:11:46 +0100

    Issue #1987 RocksDB plugin cannot share account_history classes directly, because of conflicts with BIP allocator when temporary (local) object shall be created for given class. Fixed bug in  --rocksdb-immediate-import option processing (always was matched).

--- libraries/plugins/rocksdb/rocksdb_api.hpp
+++ libraries/plugins/rocksdb/rocksdb_api.hpp
@@ -18,10 +18,66 @@ class rocksdb_plugin;
 
 namespace detail {class api_impl;}
 
+typedef std::vector<char> serialize_buffer_t;
+
+/** Dedicated definition is needed because of conflict of BIP allocator
+ *  against usage of this class as temporary object. 
+ *  The conflict appears in original associated_ops container type definition,
+ *  which in BIP version needs an allocator during constructor call.
+ */
+class tmp_account_history_object
+{
+public:
+   typedef std::vector<size_t> operation_container;
+
+   steem::protocol::account_name_type account;
+
+   const operation_container& get_ops() const
+   {
+      return associated_ops;
+   }
+
+   void store_operation_id(size_t opId)
+   {
+      associated_ops.push_back(opId);
+   }
+
+private:
+   /** Holds IDs of operations somehow involving given accout.
+    *  List head points to oldest operation, tail to newest one.
+    *  Container order is determined by operation processing.
+    */
+   operation_container associated_ops;
+   template <class T> friend struct fc::reflector;
+};
+
+/** Dedicated definition is needed because of conflict of BIP allocator
+ *  against usage of this class as temporary object. 
+ *  The conflict appears in original serialized_op container type definition,
+ *  which in BIP version needs an allocator during constructor call.
+ */
+class tmp_operation_object
+{
+public:
+   size_t               id = 0;
+
+   chain::transaction_id_type  trx_id;
+   uint32_t             block = 0;
+   uint32_t             trx_in_block = 0;
+   /// Seems that this member is never used
+   //uint16_t             op_in_trx = 0;
+   /// Seems that this member is never modified
+   //uint64_t           virtual_op = 0;
+   uint64_t             virtual_op() const { return 0;}
+   time_point_sec       timestamp;
+   serialize_buffer_t   serialized_op;
+
+};
+
 struct api_operation_object
 {
    api_operation_object() = default;
-   api_operation_object( const steem::chain::operation_object& op_obj ) :
+   api_operation_object( const tmp_operation_object& op_obj ) :
       trx_id( op_obj.trx_id ),
       block( op_obj.block ),
       trx_in_block( op_obj.trx_in_block ),
@@ -79,6 +135,10 @@ class rocksdb_api final
 };
 
 } } } // steem::plugins::rocksdb
+
+FC_REFLECT( steem::plugins::rocksdb::tmp_operation_object, (id)(trx_id)(block)(trx_in_block)(timestamp)(serialized_op) )
+
+FC_REFLECT( steem::plugins::rocksdb::tmp_account_history_object, (account)(associated_ops) )
 
 FC_REFLECT( steem::plugins::rocksdb::api_operation_object,
    (trx_id)(block)(trx_in_block)(op_in_trx)(virtual_op)(timestamp)(op) )
