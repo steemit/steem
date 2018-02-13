bb87f5c0ce8f68c7e4cf176b222f95ef587f2054
Marek Kochanowicz <mkochanowicz@steemit.com>
2018-02-13 12:24:41 +0100

    add virtual dtors to evaluator/evaluator_impl classes

--- libraries/chain/include/steem/chain/evaluator.hpp
+++ libraries/chain/include/steem/chain/evaluator.hpp
@@ -10,6 +10,8 @@ template< typename OperationType=steem::protocol::operation >
 class evaluator
 {
    public:
+      virtual ~evaluator() {}
+
       virtual void apply(const OperationType& op) = 0;
       virtual int get_type()const = 0;
 };
@@ -23,6 +25,8 @@ class evaluator_impl : public evaluator<OperationType>
 
       evaluator_impl( database& d )
          : _db(d) {}
+
+      virtual ~evaluator_impl() {}
 
       virtual void apply(const OperationType& o) final override
       {
