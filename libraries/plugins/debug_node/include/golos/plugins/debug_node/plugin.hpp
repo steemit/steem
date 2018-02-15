aa746ce0a09d1011e8ad0767439b0c59952165b3
AKorpusenko <antokorp@gmail.com>
2018-02-15 09:21:55 +0300

    Supported old api in debug_node. #373

--- libraries/plugins/debug_node/include/golos/plugins/debug_node/plugin.hpp
+++ libraries/plugins/debug_node/include/golos/plugins/debug_node/plugin.hpp
@@ -29,94 +29,17 @@ inline void disconnect_signal( boost::signals2::connection& signal )
 
 using golos::plugins::json_rpc::void_type;
 using golos::plugins::json_rpc::msg_pack;
-
-
-// debug_generate_blocks
-struct debug_generate_blocks_a {
-    std::string                               debug_key;
-    uint32_t                                  count = 0;
-    uint32_t                                  skip = golos::chain::database::skip_nothing;
-    uint32_t                                  miss_blocks = 0;
-    bool                                      edit_if_needed = true;
-};
-
-struct debug_generate_blocks_r {
-   uint32_t                                  blocks = 0;
-};
-
-
-// debug_push_blocks
-struct debug_push_blocks_a
-{
-    std::string                               src_filename;
-    uint32_t                                  count;
-    bool                                      skip_validate_invariants = false;
-};
-
-struct debug_push_blocks_r
-{
-    uint32_t                                  blocks;
-};
-
-
-// debug_generate_blocks_until
-struct debug_generate_blocks_until_a
-{
-    std::string                               debug_key;
-    fc::time_point_sec                        head_block_time;
-    bool                                      generate_sparsely = true;
-};
-
-typedef debug_push_blocks_r debug_generate_blocks_until_r;
-
-
-// debug_pop_block
-typedef void_type debug_pop_block_a;
-// struct debug_pop_block_a {
-
-// };
-
-struct debug_pop_block_r {
-    fc::optional< protocol::signed_block > block;
-};
-
 using golos::chain::witness_schedule_object;
 
-// debug_get_witness_schedule
-typedef void_type debug_get_witness_schedule_a;
-typedef witness_schedule_object debug_get_witness_schedule_r;
-
-
-// debug_get_hardfork_property_object
-
-// typedef void_type debug_get_hardfork_property_object_a;
-// typedef hardfork_property_api_object debug_get_hardfork_property_object_r;
-
-// debug_set_hardfork
-struct debug_set_hardfork_a {
-   uint32_t hardfork_id;
-};
-
-typedef void_type debug_set_hardfork_r;
-
-
-// debug_set_hardfork
-typedef debug_set_hardfork_a debug_has_hardfork_a;
-
-struct debug_has_hardfork_r {
-   bool has_hardfork;
-};
-
-
 // DEFINE API ARGS
-DEFINE_API_ARGS ( debug_generate_blocks,              msg_pack,   debug_generate_blocks_r               )
-DEFINE_API_ARGS ( debug_generate_blocks_until,        msg_pack,   debug_generate_blocks_until_r         )
-DEFINE_API_ARGS ( debug_push_blocks,                  msg_pack,   debug_push_blocks_r                   )
-DEFINE_API_ARGS ( debug_pop_block,                    msg_pack,   debug_pop_block_r                     )
-DEFINE_API_ARGS ( debug_get_witness_schedule,         msg_pack,   debug_get_witness_schedule_r          )
+DEFINE_API_ARGS ( debug_generate_blocks,              msg_pack,   uint32_t                                      )
+DEFINE_API_ARGS ( debug_generate_blocks_until,        msg_pack,   uint32_t                                      )
+DEFINE_API_ARGS ( debug_push_blocks,                  msg_pack,   uint32_t                                      )
+DEFINE_API_ARGS ( debug_pop_block,                    msg_pack,   fc::optional< protocol::signed_block >        )
+DEFINE_API_ARGS ( debug_get_witness_schedule,         msg_pack,   witness_schedule_object                       )
 // DEFINE_API_ARGS ( debug_get_hardfork_property_object, msg_pack,   debug_get_hardfork_property_object_r  )
-DEFINE_API_ARGS ( debug_set_hardfork,                 msg_pack,   debug_set_hardfork_r                  )
-DEFINE_API_ARGS ( debug_has_hardfork,                 msg_pack,   debug_has_hardfork_r                  );
+DEFINE_API_ARGS ( debug_set_hardfork,                 msg_pack,   void                                         )
+DEFINE_API_ARGS ( debug_has_hardfork,                 msg_pack,   bool                                          );
 // 
 
 
@@ -175,6 +98,10 @@ public:
 
     void save_debug_updates( fc::mutable_variant_object& target );
     void load_debug_updates( const fc::variant_object& target );
+    void debug_update( 
+        std::function< void( golos::chain::database& ) > callback,
+        uint32_t skip = golos::chain::database::skip_nothing
+    );
 
 
 private:
@@ -182,58 +109,9 @@ private:
 
     void apply_debug_updates();
 
-    std::map<protocol::public_key_type, fc::ecc::private_key> _private_keys;
-
-
-    // boost::signals2::scoped_connection _applied_block_conn;
-    // boost::signals2::scoped_connection _changed_objects_conn;
-    // boost::signals2::scoped_connection _removed_objects_conn;
-
-    std::vector< std::string > _edit_scripts;
-    //std::map< protocol::block_id_type, std::vector< fc::variant_object > > _debug_updates;
-
     struct plugin_impl;
 
     std::unique_ptr<plugin_impl> my;
 };
 
 } } } // golos::plugins::debug_node
-
-FC_REFLECT( (golos::plugins::debug_node::debug_generate_blocks_a),
-            (debug_key)
-            (count)
-            (skip)
-            (miss_blocks)
-            (edit_if_needed)
-          )
-FC_REFLECT( (golos::plugins::debug_node::debug_generate_blocks_r),
-            (blocks)
-          )
-
-FC_REFLECT( (golos::plugins::debug_node::debug_push_blocks_a),
-            (src_filename)(count)(skip_validate_invariants)
-          )
-
-FC_REFLECT( (golos::plugins::debug_node::debug_push_blocks_r),
-            (blocks)
-          )
-
-FC_REFLECT( (golos::plugins::debug_node::debug_generate_blocks_until_a),
-            (debug_key)(head_block_time)(generate_sparsely)
-          )
-
-// FC_REFLECT( (golos::plugins::debug_node::debug_pop_block_a),
-            // ()
-          // )
-
-FC_REFLECT( (golos::plugins::debug_node::debug_pop_block_r),
-            (block)
-          )
-
-FC_REFLECT( (golos::plugins::debug_node::debug_set_hardfork_a),
-            (hardfork_id)
-          )
-
-FC_REFLECT( (golos::plugins::debug_node::debug_has_hardfork_r),
-            (has_hardfork)
-          )
--- libraries/plugins/debug_node/plugin.cpp
+++ libraries/plugins/debug_node/plugin.cpp
@@ -18,9 +18,6 @@
 #include <sstream>
 #include <string>
 
-// #define CHECK_ARG_SIZE(s) \
-//    FC_ASSERT( args.args->size() == s, "Expected #s argument(s), was ${n}", ("n", args.args->size()) );
-
 namespace golos { namespace plugins { namespace debug_node {
 
 using namespace golos::chain;
@@ -31,37 +28,39 @@ public:
     }
 
     // APIs
-    debug_generate_blocks_r                debug_generate_blocks               (debug_generate_blocks_a & args);
-    debug_push_blocks_r                    debug_push_blocks                   (debug_push_blocks_a & args);
-    debug_generate_blocks_until_r          debug_generate_blocks_until         (debug_generate_blocks_until_a & args);
-    debug_pop_block_r                      debug_pop_block                     (debug_pop_block_a & args);
-    debug_get_witness_schedule_r           debug_get_witness_schedule          (debug_get_witness_schedule_a & args);
-    debug_set_hardfork_r                   debug_set_hardfork                  (debug_set_hardfork_a & args);
-    debug_has_hardfork_r                   debug_has_hardfork                  (debug_has_hardfork_a & args);
+    uint32_t debug_generate_blocks(
+        std::string debug_key,
+        uint32_t count = 0,
+        uint32_t skip = golos::chain::database::skip_nothing,
+        uint32_t miss_blocks = 0,
+        bool edit_if_needed = true
+    );
+
+    uint32_t debug_push_blocks(
+        std::string src_filename,
+        uint32_t count,
+        bool skip_validate_invariants = false
+    );
+
+    uint32_t debug_generate_blocks_until(
+        std::string debug_key,
+        fc::time_point_sec head_block_time,
+        bool generate_sparsely = true
+    );
+
+    fc::optional< protocol::signed_block > debug_pop_block();
+    witness_schedule_object debug_get_witness_schedule();
+    void debug_set_hardfork( uint32_t hardfork_id );
+    bool debug_has_hardfork( uint32_t hardfork_id );
     //
     golos::chain::database &database() {
       return db_;
     }
 
-    template< typename Lambda >
-    void debug_update( Lambda&& callback, uint32_t skip = golos::chain::database::skip_nothing )
-    {
-        // this was a method on database in Graphene
-        golos::chain::database& db = database();
-        golos::chain::block_id_type head_id = db.head_block_id();
-        auto it = _debug_updates.find( head_id );
-        if( it == _debug_updates.end() ) {
-            it = _debug_updates.emplace( head_id, std::vector< std::function< void( golos::chain::database& ) > >() ).first;
-        }
-        it->second.emplace_back( callback );
-
-        fc::optional<golos::chain::signed_block> head_block = db.fetch_block_by_id( head_id );
-        FC_ASSERT( head_block.valid() );
-
-        // What the last block does has been changed by adding to node_property_object, so we have to re-apply it
-        db.pop_block();
-        db.push_block( *head_block, skip );
-    }
+    void debug_update(
+        std::function< void( golos::chain::database& ) > callback,
+        uint32_t skip = golos::chain::database::skip_nothing
+    );
 
     void apply_debug_updates();
     void on_applied_block( const protocol::signed_block & b );
@@ -69,6 +68,15 @@ public:
 
     // void save_debug_updates( fc::mutable_variant_object& target );
     // void load_debug_updates( const fc::variant_object& target );
+    std::map<protocol::public_key_type, fc::ecc::private_key> _private_keys;
+
+
+    // boost::signals2::scoped_connection _applied_block_conn;
+    // boost::signals2::scoped_connection _changed_objects_conn;
+    // boost::signals2::scoped_connection _removed_objects_conn;
+
+    std::vector< std::string > _edit_scripts;
+    //std::map< protocol::block_id_type, std::vector< fc::variant_object > > _debug_updates;
 
     bool logging = true;
 
@@ -103,13 +111,13 @@ void plugin::plugin_initialize( const boost::program_options::variables_map& opt
     my.reset(new plugin_impl);
 
     if( options.count( "debug-node-edit-script" ) > 0 ) {
-        _edit_scripts = options.at( "debug-node-edit-script" ).as< std::vector< std::string > >();
+        my->_edit_scripts = options.at( "debug-node-edit-script" ).as< std::vector< std::string > >();
     }
 
     if( options.count("edit-script") > 0 ) {
         wlog( "edit-scripts is deprecated in favor of debug-node-edit-script" );
         auto scripts = options.at( "edit-script" ).as< std::vector< std::string > >();
-        _edit_scripts.insert( _edit_scripts.end(), scripts.begin(), scripts.end() );
+        my->_edit_scripts.insert(my->_edit_scripts.end(), scripts.begin(), scripts.end() );
     }
 
     // connect needed signals
@@ -138,6 +146,33 @@ void plugin::plugin_shutdown() {
       _json_object_stream.reset();
    }*/
    return;
+}
+void plugin::debug_update (
+        std::function< void( golos::chain::database& ) > callback,
+        uint32_t skip
+) {
+    my->debug_update(callback, skip);
+}
+
+void plugin::plugin_impl::debug_update (
+        std::function< void( golos::chain::database& ) > callback,
+        uint32_t skip 
+    ) {
+    // this was a method on database in Graphene
+    golos::chain::database& db = database();
+    golos::chain::block_id_type head_id = db.head_block_id();
+    auto it = _debug_updates.find( head_id );
+    if( it == _debug_updates.end() ) {
+        it = _debug_updates.emplace( head_id, std::vector< std::function< void( golos::chain::database& ) > >() ).first;
+    }
+    it->second.emplace_back( callback );
+
+    fc::optional<golos::chain::signed_block> head_block = db.fetch_block_by_id( head_id );
+    FC_ASSERT( head_block.valid() );
+
+    // What the last block does has been changed by adding to node_property_object, so we have to re-apply it
+    db.pop_block();
+    db.push_block( *head_block, skip );
 }
 
 void plugin::plugin_impl::apply_debug_updates() {
@@ -169,18 +204,24 @@ void plugin::on_applied_block( const protocol::signed_block & b ) {
     my->on_applied_block(b);
 }
 
-debug_generate_blocks_r plugin::plugin_impl::debug_generate_blocks(debug_generate_blocks_a & args) {
-    debug_generate_blocks_r ret;
-    if( args.count == 0 ) {
-        ret.blocks = 0;
+uint32_t plugin::plugin_impl::debug_generate_blocks(
+    std::string debug_key,
+    uint32_t count,
+    uint32_t skip,
+    uint32_t miss_blocks,
+    bool edit_if_needed 
+) {
+    uint32_t ret;
+    if( count == 0 ) {
+        ret = 0;
         return ret;
     }
 
     fc::optional<fc::ecc::private_key> debug_private_key;
     golos::chain::public_key_type debug_public_key;
 
-    if( args.debug_key != "" ) {
-        debug_private_key = golos::utilities::wif_to_key( args.debug_key );
+    if( debug_key != "" ) {
+        debug_private_key = golos::utilities::wif_to_key( debug_key );
         FC_ASSERT( debug_private_key.valid() );
         debug_public_key = debug_private_key->get_public_key();
     }
@@ -188,14 +229,14 @@ debug_generate_blocks_r plugin::plugin_impl::debug_generate_blocks(debug_generat
         if( logging ) {
             elog( "Skipping generation because I don't know the private key");
         }
-        ret.blocks = 0;
+        ret = 0;
         return ret;
     }
 
     golos::chain::database& db = database();
-    uint32_t slot = args.miss_blocks+1, produced = 0;
-    while( produced < args.count ) {
-        uint32_t new_slot = args.miss_blocks+1;
+    uint32_t slot = miss_blocks + 1, produced = 0;
+    while( produced < count ) {
+        uint32_t new_slot = miss_blocks + 1;
         std::string scheduled_witness_name = db.get_scheduled_witness( slot );
         fc::time_point_sec scheduled_time = db.get_slot_time( slot );
         const golos::chain::witness_object& scheduled_witness = db.get_witness( scheduled_witness_name );
@@ -205,7 +246,7 @@ debug_generate_blocks_r plugin::plugin_impl::debug_generate_blocks(debug_generat
             ("sk", scheduled_key)("dk", debug_public_key)("sl", slot)("t", scheduled_time) );
         }
         if( scheduled_key != debug_public_key ) {
-            if( args.edit_if_needed ) {
+            if( edit_if_needed ) {
                 if( logging ) {
                     wlog( "Modified key for witness ${w}", ("w", scheduled_witness_name) );
                 }
@@ -215,72 +256,61 @@ debug_generate_blocks_r plugin::plugin_impl::debug_generate_blocks(debug_generat
                    {
                       w.signing_key = debug_public_key;
                    });
-                }, args.skip );
+                }, skip );
             }
             else {
                 break;
             }
         }
 
-        db.generate_block( scheduled_time, scheduled_witness_name, *debug_private_key, args.skip );
+        db.generate_block( scheduled_time, scheduled_witness_name, *debug_private_key, skip );
         ++produced;
         slot = new_slot;
     }
 
-    ret.blocks = produced;
+    ret = produced;
     return ret;
 }
 
 
-debug_generate_blocks_until_r plugin::plugin_impl::debug_generate_blocks_until(debug_generate_blocks_until_a & args) {
-    const std::string& debug_key = args.debug_key;
-    const fc::time_point_sec& head_block_time = args.head_block_time;
-    bool generate_sparsely = args.generate_sparsely;
+uint32_t plugin::plugin_impl::debug_generate_blocks_until(
+    std::string debug_key,
+    fc::time_point_sec head_block_time,
+    bool generate_sparsely
+) {
     uint32_t skip = golos::chain::database::skip_nothing;
     
     golos::chain::database& db = database();
 
     if( db.head_block_time() >= head_block_time ) {
-        return {0};
+        return 0;
     }
 
     uint32_t new_blocks = 0;
 
     if( generate_sparsely ) {
-        debug_generate_blocks_a tmp_args;
-
-        tmp_args.debug_key = debug_key;
-        tmp_args.count = 1;
-        tmp_args.skip = skip;
-
-        new_blocks += debug_generate_blocks( tmp_args ).blocks;
+        new_blocks += debug_generate_blocks( debug_key, 1, skip);
         auto slots_to_miss = db.get_slot_at_time( head_block_time );
         if( slots_to_miss > 1 ) {
             slots_to_miss--;
-            tmp_args.miss_blocks = slots_to_miss;
-            new_blocks += debug_generate_blocks( tmp_args ).blocks;
+            new_blocks += debug_generate_blocks( debug_key, 1, skip, slots_to_miss);
         }
     }
     else {
         while( db.head_block_time() < head_block_time ) {
-            debug_generate_blocks_a tmp_args;
-
-            tmp_args.debug_key = debug_key;
-            tmp_args.count = 1;
-
-            new_blocks += debug_generate_blocks( tmp_args ).blocks;
+            new_blocks += debug_generate_blocks( debug_key, 1 );
         }
     }
-    return {new_blocks};
+    return new_blocks;
 }
 
-debug_push_blocks_r plugin::plugin_impl::debug_push_blocks(debug_push_blocks_a & args) {
-    auto& src_filename = args.src_filename;
-    auto count = args.count;
-    auto skip_validate_invariants = args.skip_validate_invariants;
-
+uint32_t plugin::plugin_impl::debug_push_blocks(
+    std::string src_filename,
+    uint32_t count,
+    bool skip_validate_invariants
+) {
     if( count == 0 ) {
-        return { 0 };
+        return 0;
     }
 
     fc::path src_path = fc::path( src_filename );
@@ -305,7 +335,7 @@ debug_push_blocks_r plugin::plugin_impl::debug_push_blocks(debug_push_blocks_a &
             uint64_t block_pos = log.get_block_pos( first_block + i );
             if( block_pos == golos::chain::block_log::npos ) {
                 wlog( "Block database ${fn} only contained ${i} of ${n} requested blocks", ("i", i)("n", count)("fn", src_filename) );
-                return { i };
+                return i ;
             }
 
             decltype( log.read_block(0) ) result;
@@ -327,17 +357,17 @@ debug_push_blocks_r plugin::plugin_impl::debug_push_blocks(debug_push_blocks_a &
             }
         }
         ilog( "Completed loading block_database successfully" );
-        return { count };
+        return count;
     }
-    return { 0 };
+    return 0;
 }
 
-debug_pop_block_r plugin::plugin_impl::debug_pop_block(debug_pop_block_a & args) {
+fc::optional< protocol::signed_block > plugin::plugin_impl::debug_pop_block() {
     auto & db = database();
-    return { db.fetch_block_by_number( db.head_block_num() ) };
+    return db.fetch_block_by_number( db.head_block_num() );
 }
 
-debug_get_witness_schedule_r plugin::plugin_impl::debug_get_witness_schedule(debug_get_witness_schedule_a & args) {
+witness_schedule_object plugin::plugin_impl::debug_get_witness_schedule( ) {
     auto & db = database();
     return db.get( golos::chain::witness_schedule_id_type() );
 }
@@ -347,19 +377,19 @@ debug_get_witness_schedule_r plugin::plugin_impl::debug_get_witness_schedule(deb
 //     return db.get( chain::hardfork_property_id_type() );
 // }
 
-debug_set_hardfork_r plugin::plugin_impl::debug_set_hardfork(debug_set_hardfork_a & args) {
-    if( args.hardfork_id > STEEMIT_NUM_HARDFORKS ) {
-        return {};
+void plugin::plugin_impl::debug_set_hardfork( uint32_t hardfork_id ) {
+    if( hardfork_id > STEEMIT_NUM_HARDFORKS ) {
+        return;
     }
 
-    database().set_hardfork( args.hardfork_id, false );
+    database().set_hardfork( hardfork_id, false );
     // TODO: Maybe there should be added a success result, because now it's hard to understand is everything is ok or no...
-    return {};
+    return;
 }
 
-debug_has_hardfork_r plugin::plugin_impl::debug_has_hardfork(debug_has_hardfork_a & args) {
+bool plugin::plugin_impl::debug_has_hardfork( uint32_t hardfork_id ) {
     auto & db = database();
-    return { db.get( golos::chain::hardfork_property_id_type() ).last_hardfork >= args.hardfork_id };
+    return db.get( golos::chain::hardfork_property_id_type() ).last_hardfork >= hardfork_id;
 }
 
 
@@ -382,7 +412,11 @@ debug_has_hardfork_r plugin::plugin_impl::debug_has_hardfork(debug_has_hardfork_
 // 
 
 DEFINE_API ( plugin, debug_generate_blocks ) {
-    debug_generate_blocks_a tmp;
+    std::string                               debug_key;
+    uint32_t                                  count = 0;
+    uint32_t                                  skip = golos::chain::database::skip_nothing;
+    uint32_t                                  miss_blocks = 0;
+    bool                                      edit_if_needed = true;
 
     FC_ASSERT(args.args.valid(), "Invalid parameters" ) ;
 
@@ -390,18 +424,18 @@ DEFINE_API ( plugin, debug_generate_blocks ) {
 
     FC_ASSERT( args_count > 0 && args_count < 5, "Wrong parameters number, given ${n}", ("n", args_count) );
     auto args_vector = *(args.args);
-    tmp.debug_key = args_vector[0].as_string();
+    debug_key = args_vector[0].as_string();
     if (args_count > 1) {
-        tmp.count = args_vector[1].as_int64();        
+        count = args_vector[1].as_int64();        
     }
     if (args_count > 2) {
-        tmp.skip = args_vector[2].as_int64();
+        skip = args_vector[2].as_int64();
     }
     if (args_count > 3) {
-        tmp.miss_blocks = args_vector[3].as_int64();
+        miss_blocks = args_vector[3].as_int64();
     }
     if (args_count > 4) {
-        tmp.edit_if_needed = args_vector[4].as_bool();
+        edit_if_needed = args_vector[4].as_bool();
     }
 
     // This causes a casting error  |
@@ -410,12 +444,14 @@ DEFINE_API ( plugin, debug_generate_blocks ) {
     // 
     auto &db = my->database();
     return db.with_read_lock([&]() {
-        return my->debug_generate_blocks(tmp);
+        return my->debug_generate_blocks( debug_key, count, skip, miss_blocks, edit_if_needed );
     });
 }
 
 DEFINE_API ( plugin, debug_push_blocks ) {
-    debug_push_blocks_a tmp;
+    std::string src_filename;
+    uint32_t count;
+    bool skip_validate_invariants = false;
 
     FC_ASSERT(args.args.valid(), "Invalid parameters" ) ;
 
@@ -424,20 +460,22 @@ DEFINE_API ( plugin, debug_push_blocks ) {
     FC_ASSERT( args_count > 0 && args_count < 5, "Wrong parameters number, given ${n}", ("n", args_count) );
     auto args_vector = *(args.args);
 
-    tmp.src_filename = args_vector[0].as_string();
-    tmp.count = args_vector[1].as_int64();        
+    src_filename = args_vector[0].as_string();
+    count = args_vector[1].as_int64();        
     if (args_count > 2) {
-        tmp.skip_validate_invariants = args_vector[3].as_bool();
+        skip_validate_invariants = args_vector[3].as_bool();
     }
 
     auto &db = my->database();
     return db.with_read_lock([&]() {
-        return my->debug_push_blocks(tmp);
+        return my->debug_push_blocks( src_filename, count, skip_validate_invariants );
     });
 }
 
 DEFINE_API ( plugin, debug_generate_blocks_until ) {
-    debug_generate_blocks_until_a tmp;
+    std::string debug_key;
+    fc::time_point_sec head_block_time;
+    bool generate_sparsely = true;
 
     FC_ASSERT(args.args.valid(), "Invalid parameters" ) ;
 
@@ -446,32 +484,30 @@ DEFINE_API ( plugin, debug_generate_blocks_until ) {
     FC_ASSERT( args_count > 0 && args_count < 5, "Wrong parameters number, given ${n}", ("n", args_count) );
     auto args_vector = *(args.args);
 
-    tmp.debug_key = args_vector[0].as_string();
-    tmp.head_block_time = fc::time_point_sec::from_iso_string( args_vector[1].as_string() );      
+    debug_key = args_vector[0].as_string();
+    head_block_time = fc::time_point_sec::from_iso_string( args_vector[1].as_string() );      
 
     if (args_count > 2) {
-        tmp.generate_sparsely = args_vector[3].as_bool();
+        generate_sparsely = args_vector[3].as_bool();
     }
 
     auto &db = my->database();
     return db.with_read_lock([&]() {
-        return my->debug_generate_blocks_until(tmp);
+        return my->debug_generate_blocks_until( debug_key, head_block_time, generate_sparsely );
     });
 }
 
 DEFINE_API ( plugin, debug_pop_block ) {
-    debug_pop_block_a tmp;
     auto &db = my->database();
     return db.with_read_lock([&]() {
-        return my->debug_pop_block(tmp);
+        return my->debug_pop_block();
     });
 }
 
 DEFINE_API ( plugin, debug_get_witness_schedule ) {
-    debug_get_witness_schedule_a tmp;
     auto &db = my->database();
     return db.with_read_lock([&]() {
-        return my->debug_get_witness_schedule(tmp);
+        return my->debug_get_witness_schedule();
     });
 }
 
@@ -484,7 +520,7 @@ DEFINE_API ( plugin, debug_get_witness_schedule ) {
 // }
 
 DEFINE_API ( plugin, debug_set_hardfork ) {
-    debug_set_hardfork_a tmp;
+    uint32_t hardfork_id;
 
     FC_ASSERT(args.args.valid(), "Invalid parameters" ) ;
 
@@ -492,16 +528,16 @@ DEFINE_API ( plugin, debug_set_hardfork ) {
 
     FC_ASSERT( args_count == 1, "Wrong parameters number, given ${n}", ("n", args_count) );
     auto args_vector = *(args.args);
-    tmp.hardfork_id = args_vector[0].as_int64();
+    hardfork_id = args_vector[0].as_int64();
 
     auto &db = my->database();
     return db.with_read_lock([&]() {
-        return my->debug_set_hardfork(tmp);
+        return my->debug_set_hardfork( hardfork_id );
     });
 }
 
 DEFINE_API ( plugin, debug_has_hardfork ) {
-    debug_has_hardfork_a tmp;
+    uint32_t hardfork_id;
 
     FC_ASSERT(args.args.valid(), "Invalid parameters" ) ;
 
@@ -509,11 +545,11 @@ DEFINE_API ( plugin, debug_has_hardfork ) {
 
     FC_ASSERT( args_count == 1, "Wrong parameters number, given ${n}", ("n", args_count) );
     auto args_vector = *(args.args);
-    tmp.hardfork_id = args_vector[0].as_int64();
+    hardfork_id = args_vector[0].as_int64();
 
     auto &db = my->database();
     return db.with_read_lock([&]() {
-        return my->debug_has_hardfork(tmp);
+        return my->debug_has_hardfork( hardfork_id );
     });
 }
 
