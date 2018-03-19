a3c9d6faaeb874d32c1da2d076c2cb0ce5c18820
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2018-03-19 12:38:13 -0700

    Fix start paas scripts not reindexing when using ramdisk

--- contrib/startpaassteemd.sh
+++ contrib/startpaassteemd.sh
@@ -80,7 +80,9 @@ if [[ "$USE_RAMDISK" ]]; then
   else
     s3cmd get s3://$S3_BUCKET/blockchain-$VERSION-latest.tar.bz2 - | pbzip2 -m2000dc | tar x --wildcards 'blockchain/block*' -C /mnt/ramdisk 'blockchain/shared*'
   fi
-  chown -R steemd:steemd /mnt/ramdisk/blockchain
+  if [[ $? -e 0 ]]; then
+    chown -R steemd:steemd /mnt/ramdisk/blockchain
+  fi
 else
   if [[ "$IS_BROADCAST_NODE" ]]; then
     s3cmd get s3://$S3_BUCKET/broadcast-$VERSION-latest.tar.bz2 - | pbzip2 -m2000dc | tar x
@@ -96,7 +98,13 @@ if [[ $? -ne 0 ]]; then
     exit 1
   else
     echo notifysteemdsync steemdsync: shared memory file for $VERSION not found, creating a new one by replaying the blockchain
-    mkdir blockchain
+    if [[ "$USE_RAMDISK" ]]; then
+      mkdir -p /mnt/ramdisk/blockchain
+      chown -R steemd:steemd /mnt/ramdisk/blockchain
+      cd /mnt/ramdisk
+    else
+      mkdir blockchain
+    fi
     aws s3 cp s3://$S3_BUCKET/block_log-latest blockchain/block_log
     if [[ $? -ne 0 ]]; then
       echo notifysteemdsync steemdsync: unable to pull latest block_log from S3, will sync from scratch.
