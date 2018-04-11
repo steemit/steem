4cf36d0c18b84944c2dfbcbbb08cc039c7a13f28
Andrew Falaleev <andrew.falaleev@gmail.com>
2018-04-11 10:25:52 +0700

    Fix get_discussions. #509

--- plugins/social_network/discussion_query.cpp
+++ plugins/social_network/discussion_query.cpp
@@ -2,23 +2,24 @@
 
 #include <golos/plugins/social_network/api_object/discussion_query.hpp>
 
-namespace golos {
-    namespace plugins {
-        namespace social_network {
+namespace golos { namespace plugins { namespace social_network {
 
-            void discussion_query::validate() const {
-                FC_ASSERT(limit <= 100);
+    void discussion_query::validate() const {
+        FC_ASSERT(limit <= 100);
+        FC_ASSERT(depth > limit);
+        FC_ASSERT(start <= depth - limit);
 
-                for (const std::set<std::string>::value_type &iterator : filter_tags) {
-                    FC_ASSERT(select_tags.find(iterator) == select_tags.end());
-                }
-
-                for (const auto &iterator : filter_languages) {
-                    FC_ASSERT(select_languages.find(iterator) == select_languages.end());
-                }
-            }
-
+        for (auto& itr : filter_tags) {
+            FC_ASSERT(select_tags.find(itr) == select_tags.end());
         }
+
+        for (auto& itr : filter_languages) {
+            FC_ASSERT(select_languages.find(itr) == select_languages.end());
+        }
+
+        FC_ASSERT(start_author.valid() == start_permlink.valid());
+
+        FC_ASSERT(parent_author.valid() == parent_permlink.valid());
     }
-}
+} } } // golos::plugins::social_network
 
--- plugins/social_network/include/golos/plugins/social_network/api_object/discussion_query.hpp
+++ plugins/social_network/include/golos/plugins/social_network/api_object/discussion_query.hpp
@@ -11,37 +11,46 @@
 #include <vector>
 #include <fc/exception/exception.hpp>
 
-namespace golos {
-    namespace plugins {
-        namespace social_network {
-            /**
-             * @class discussion_query
-             * @brief The discussion_query structure implements the RPC API param set.
-             *  Defines the arguments to a query as a struct so it can be easily extended
-             */
+#ifndef DEFAULT_VOTE_LIMIT
+#  define DEFAULT_VOTE_LIMIT 10000
+#endif
 
-            class discussion_query {
-            public:
-                void validate() const;
+#ifndef DEFAULT_QUERY_DEPTH
+#  define DEFAULT_QUERY_DEPTH 100000
+#endif
 
-                uint32_t                  limit = 0; ///< the discussions return amount top limit
-                std::set<std::string>     select_authors; ///< list of authors to select
-                std::set<std::string>     select_tags; ///< list of tags to include, posts without these tags are filtered
-                std::set<std::string>     filter_tags; ///< list of tags to exclude, posts with these tags are filtered;
-                uint32_t                  truncate_body = 0; ///< the amount of bytes of the post body to return, 0 for all
-                fc::optional<std::string> start_author; ///< the author of discussion to start searching from
-                fc::optional<std::string> start_permlink; ///< the permlink of discussion to start searching from
-                fc::optional<std::string> parent_author; ///< the author of parent discussion
-                fc::optional<std::string> parent_permlink; ///< the permlink of parent discussion
-                std::set<std::string>     select_languages; ///< list of language to select
-                std::set<std::string>     filter_languages; ///< list of language to filter
-            };
-        }
-    }
-}
+namespace golos { namespace plugins { namespace social_network {
+    /**
+     * @class discussion_query
+     * @brief The discussion_query structure implements the RPC API param set.
+     *  Defines the arguments to a query as a struct so it can be easily extended
+     */
 
-FC_REFLECT((golos::plugins::social_network::discussion_query),
-           (select_tags)(filter_tags)(select_authors)(truncate_body)(start_author)(start_permlink)(parent_author)(
-                   parent_permlink)(limit)(select_languages)(filter_languages));
+    class discussion_query {
+    public:
+        void validate() const;
+
+        uint32_t                  start = 0; ///< the discussions from position
+        uint32_t                  limit = 0; ///< the discussions return amount top limit
+        uint32_t                  depth = DEFAULT_QUERY_DEPTH; ///< maximum depth of searching
+        std::set<std::string>     select_tags; ///< list of tags to include, posts without these tags are filtered
+        std::set<std::string>     filter_tags; ///< list of tags to exclude, posts with these tags are filtered;
+        std::set<std::string>     select_languages; ///< list of language to select
+        std::set<std::string>     filter_languages; ///< list of language to filter
+        uint32_t                  truncate_body = 0; ///< the amount of bytes of the post body to return, 0 for all
+        uint32_t                  vote_limit = DEFAULT_VOTE_LIMIT; ///< limit for active votes
+        std::set<std::string>     select_authors; ///< list of authors to select
+        fc::optional<std::string> start_author; ///< the author of discussion to start searching from
+        fc::optional<std::string> start_permlink; ///< the permlink of discussion to start searching from
+        fc::optional<std::string> parent_author; ///< the author of parent discussion
+        fc::optional<std::string> parent_permlink; ///< the permlink of parent discussion
+    };
+} } } // golos::plugins::social_network
+
+FC_REFLECT(
+    (golos::plugins::social_network::discussion_query),
+        (select_tags)(filter_tags)(select_authors)(truncate_body)(vote_limit)
+        (start_author)(start_permlink)(parent_author)
+        (parent_permlink)(start)(limit)(select_languages)(filter_languages));
 
 #endif //GOLOS_DISCUSSION_QUERY_H
