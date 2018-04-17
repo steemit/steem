9d0c5e548e982f189475d4b7a42ed3e37eb6bd91
zxcat <550974+zxcat@users.noreply.github.com>
2018-04-18 01:48:44 +0300

    Add delegation ops base (without evaluators) #535

--- libraries/protocol/steem_operations.cpp
+++ libraries/protocol/steem_operations.cpp
@@ -1,8 +1,7 @@
 #include <golos/protocol/steem_operations.hpp>
 #include <fc/io/json.hpp>
 
-namespace golos {
-    namespace protocol {
+namespace golos { namespace protocol {
 
         /// TODO: after the hardfork, we can rename this method validate_permlink because it is strictily less restrictive than before
         ///  Issue #56 contains the justificiation for allowing any UTF-8 string to serve as a permlink, content will be grouped by tags
@@ -33,6 +32,23 @@ namespace golos {
             }
             FC_ASSERT(fee >=
                       asset(0, STEEM_SYMBOL), "Account creation fee cannot be negative");
+        }
+
+        void account_create_with_delegation_operation::validate() const {
+            validate_account_name(new_account_name);
+            validate_account_name(creator);
+            FC_ASSERT(is_asset_type(fee, STEEM_SYMBOL), "Account creation fee must be GOLOS");
+            FC_ASSERT(is_asset_type(delegation, VESTS_SYMBOL), "Delegation must be VESTS");
+            FC_ASSERT(fee >= asset(0, STEEM_SYMBOL), "Account creation fee cannot be negative");
+            FC_ASSERT(delegation >= asset(0, VESTS_SYMBOL), "Delegation cannot be negative");
+            owner.validate();
+            active.validate();
+            posting.validate();
+
+            if (json_metadata.size() > 0) {
+                FC_ASSERT(fc::is_utf8(json_metadata), "JSON Metadata not formatted in UTF8");
+                FC_ASSERT(fc::json::is_valid(json_metadata), "JSON Metadata not valid JSON");
+            }
         }
 
         void account_update_operation::validate() const {
@@ -534,6 +550,11 @@ namespace golos {
                       reset_account, "new reset account cannot be current reset account");
         }
 
+        void delegate_vesting_shares_operation::validate() const {
+            validate_account_name(delegator);
+            validate_account_name(delegatee);
+            FC_ASSERT(is_asset_type(vesting_shares, VESTS_SYMBOL), "Delegation must be VESTS");
+            FC_ASSERT(vesting_shares >= asset(0, VESTS_SYMBOL), "Delegation cannot be negative");
+        }
 
-    }
-} // golos::protocol
+} } // golos::protocol
