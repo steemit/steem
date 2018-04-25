0c6d1455b35bffc088244da67b025b4a3600d62f
AKorpusenko <antokorp@gmail.com>
2018-04-25 12:02:57 +0300

    Added applied_operation to chain. #573

---
+++ libraries/chain/applied_operation.cpp
@@ -0,0 +1,15 @@
+#include <golos/chain/applied_operation.hpp>
+
+namespace golos {
+    namespace chain {
+        applied_operation::applied_operation() {
+        }
+
+        applied_operation::applied_operation(const chain::operation_object &op_obj) : trx_id(op_obj.trx_id),
+                block(op_obj.block), trx_in_block(op_obj.trx_in_block), op_in_trx(op_obj.op_in_trx),
+                virtual_op(op_obj.virtual_op), timestamp(op_obj.timestamp) {
+            //fc::raw::unpack( op_obj.serialized_op, op );     // g++ refuses to compile this as ambiguous
+            op = fc::raw::unpack<protocol::operation>(op_obj.serialized_op);
+        }
+    }
+}
---
+++ libraries/chain/include/golos/chain/applied_operation.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <golos/protocol/operations.hpp>
+#include <golos/chain/steem_object_types.hpp>
+#include <golos/chain/history_object.hpp>
+
+namespace golos {
+    namespace chain {
+
+        struct applied_operation {
+            applied_operation();
+
+            applied_operation(const golos::chain::operation_object &op_obj);
+
+            golos::protocol::transaction_id_type trx_id;
+            uint32_t block = 0;
+            uint32_t trx_in_block = 0;
+            uint16_t op_in_trx = 0;
+            uint64_t virtual_op = 0;
+            fc::time_point_sec timestamp;
+            golos::protocol::operation op;
+        };
+
+    }
+}
+
+FC_REFLECT((golos::chain::applied_operation),
+    (trx_id)
+    (block)
+    (trx_in_block)
+    (op_in_trx)
+    (virtual_op)
+    (timestamp)
+    (op)
+)
