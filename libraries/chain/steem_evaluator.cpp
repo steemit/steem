7a81522d78a558b0445a3fe8316bdcbcfaee0c8b
Łukasz Bujak <eu-be@tlen.pl>
2018-05-21 13:11:42 +0200

    Replaced 20s comment limit with 3s (1 block) #2019

--- libraries/chain/steem_evaluator.cpp
+++ libraries/chain/steem_evaluator.cpp
@@ -613,14 +613,24 @@ void comment_evaluator::do_apply( const comment_operation& o )
          if( o.parent_author == STEEM_ROOT_POST_PARENT )
              FC_ASSERT( ( now - auth.last_root_post ) > STEEM_MIN_ROOT_COMMENT_INTERVAL, "You may only post once every 5 minutes.", ("now",now)("last_root_post", auth.last_root_post) );
          else
-             FC_ASSERT( (now - auth.last_post) > STEEM_MIN_REPLY_INTERVAL, "You may only comment once every 20 seconds.", ("now",now)("auth.last_post",auth.last_post) );
+         {
+            if( _db.has_hardfork( STEEM_HARDFORK_0_20__2019 ) )
+               FC_ASSERT( (now - auth.last_post) > STEEM_MIN_REPLY_INTERVAL, "You may only comment once every 3 seconds.", ("now",now)("auth.last_post",auth.last_post) );
+            else
+               FC_ASSERT( (now - auth.last_post) > STEEM_MIN_REPLY_INTERVAL_PRE_HF20, "You may only comment once every 20 seconds.", ("now",now)("auth.last_post",auth.last_post) );
+         }
       }
       else if( _db.has_hardfork( STEEM_HARDFORK_0_6__113 ) )
       {
          if( o.parent_author == STEEM_ROOT_POST_PARENT )
              FC_ASSERT( (now - auth.last_post) > STEEM_MIN_ROOT_COMMENT_INTERVAL, "You may only post once every 5 minutes.", ("now",now)("auth.last_post",auth.last_post) );
          else
-             FC_ASSERT( (now - auth.last_post) > STEEM_MIN_REPLY_INTERVAL, "You may only comment once every 20 seconds.", ("now",now)("auth.last_post",auth.last_post) );
+         {
+            if( _db.has_hardfork( STEEM_HARDFORK_0_20__2019 ) )
+               FC_ASSERT( (now - auth.last_post) > STEEM_MIN_REPLY_INTERVAL, "You may only comment once every 3 seconds.", ("now",now)("auth.last_post",auth.last_post) );
+            else
+               FC_ASSERT( (now - auth.last_post) > STEEM_MIN_REPLY_INTERVAL_PRE_HF20, "You may only comment once every 20 seconds.", ("now",now)("auth.last_post",auth.last_post) );
+         }
       }
       else
       {
