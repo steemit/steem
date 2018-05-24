5188aee280f1a09febe8a564d5e99a27dc566811
Marek Kochanowicz <mkochanowicz@steemit.com>
2018-05-24 17:23:20 +0200

    add key extractors: member and const_mem_fun (Issue #2460)

--- libraries/chainbase/include/chainbase/util/bmi_index_specifiers.hpp
+++ libraries/chainbase/include/chainbase/util/bmi_index_specifiers.hpp
@@ -42,6 +42,52 @@ using ordered_unique = ordered_index_specifier<true, Args...>;
 template <typename... Args>
 using ordered_nonunique = ordered_index_specifier<false, Args...>;
 
+namespace detail
+{
+template <class Class, typename Type, Type Class::*PtrToMember>
+struct const_member
+{
+   Type& operator() (const Class& x) const
+      { return x.*PtrToMember; }
+};
+
+template <class Class, typename Type, Type Class::*PtrToMember>
+struct non_const_member
+{
+   const Type& operator() (const Class& x) const
+      { return x.*PtrToMember; }
+   Type& operator() (Class& x) const
+      { return x.*PtrToMember; }
+};
+} // namespace detail
+
+template <class Class, typename Type, Type Class::*PtrToMember>
+struct member : std::conditional_t<std::is_const<Type>::value,
+                                   detail::const_member<Class, Type, PtrToMember>,
+                                   detail::non_const_member<Class, Type, PtrToMember>>
+{
+   typedef Type result_type;
+
+   template <typename ChainedPtr>
+   std::enable_if_t<!std::is_convertible<const ChainedPtr&, const Class&>::value, Type&>
+   operator() (const ChainedPtr& x) const
+      { return operator()(*x); }
+};
+
+template <class Class, typename Type, Type (Class::*PtrToMemberFunction)() const>
+struct const_mem_fun
+{
+   typedef typename remove_reference<Type>::type result_type;
+
+   template <typename ChainedPtr>
+   std::enable_if_t<std::is_convertible<const ChainedPtr&, const Class&>, Type>
+   operator() (const ChainedPtr& x) const
+      { return operator()(*x); }
+
+  Type operator() (const Class& x) const
+   { return (x.*PtrToMemberFunction)(); }
+};
+
 } /// namespace bmi
 
 #endif /// __BMI_INDEX_SPECIFIERS_HPP
