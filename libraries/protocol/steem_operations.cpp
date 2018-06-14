2d249b189c706bf1d1349e1f18dc555c0a1771dd
drakos <4613678+Jolly-Pirate@users.noreply.github.com>
2018-06-14 18:57:01 -0400

    Increase the minimum transfer amount
    Make it more expensive for spammers to send memos.

--- libraries/protocol/steem_operations.cpp
+++ libraries/protocol/steem_operations.cpp
@@ -159,7 +159,7 @@ namespace steemit { namespace protocol {
       validate_account_name( from );
       validate_account_name( to );
       FC_ASSERT( amount.symbol != VESTS_SYMBOL, "transferring of Steem Power (STMP) is not allowed." );
-      FC_ASSERT( amount.amount > 0, "Cannot transfer a negative amount (aka: stealing)" );
+      FC_ASSERT( amount.amount > 0.099, "Minimum transaction amount is 0.100" );
       FC_ASSERT( memo.size() < STEEMIT_MAX_MEMO_SIZE, "Memo is too large" );
       FC_ASSERT( fc::is_utf8( memo ), "Memo is not UTF8" );
    } FC_CAPTURE_AND_RETHROW( (*this) ) }
