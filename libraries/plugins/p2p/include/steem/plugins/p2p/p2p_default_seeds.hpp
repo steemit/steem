912f91e22877d5fc950404718faf510ccee59463
TimCliff <dryseed@hotmail.com>
2018-07-09 10:23:32 -0500

    Remove seed nodes that are down #2580

--- libraries/plugins/p2p/include/steem/plugins/p2p/p2p_default_seeds.hpp
+++ libraries/plugins/p2p/include/steem/plugins/p2p/p2p_default_seeds.hpp
@@ -35,19 +35,15 @@ const std::vector< std::string > default_seeds = {
    "seed.steemian.info:2001",             // drakos
    "seed.followbtcnews.com:2001",         // followbtcnews
    "node.mahdiyari.info:2001",            // mahdiyari
-   "seed.jerrybanfield.com:2001",         // jerrybanfield
    "seed.curiesteem.com:2001",            // curie
    "seed.riversteem.com:2001",            // riverhead
-   "steem-seed.furion.me:2001",           // furion
    "148.251.237.104:2001",                // steem-bounty
    "seed1.blockbrothers.io:2001",         // blockbrothers
    "steemseed-fin.privex.io:2001",        // privex
    "seed.jamzed.pl:2001",                 // jamzed
-   "seed.steem.prcolaco.com:2001",        // prc
    "seed1.cryptobot.news:2001",           // libertyranger
    "seed.thecryptodrive.com:2001",        // thecryptodrive
    "seed.brandonfrye.us:2001",            // brandonfrye
-   "seed.minnowshares.net:2001",          // reggaemuffin
    "seed.firepower.ltd:2001"              // firepower
 };
 #endif
