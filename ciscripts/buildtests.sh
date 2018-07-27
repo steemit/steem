9b779a3efb2ce334421d679c98e2c2347ed1920a
justinw <juswelch@gmail.com>
2018-07-27 11:13:08 -0400

    remove copy cobertura dir to workspace

--- ciscripts/buildtests.sh
+++ ciscripts/buildtests.sh
@@ -2,4 +2,3 @@
 set -e
 sudo docker build --build-arg BUILD_STEP=1 -t=steemit/steem:tests .
 sudo docker run -v $WORKSPACE:/var/jenkins steemit/steem:tests cp -r /var/cobertura /var/jenkins
-cp -r $WORKSPACE/cobertura .
