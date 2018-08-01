5b71203b3f3f66d8ae02ade7b76466679375e419
theoreticalbts <theoreticalbts@users.noreply.github.com>
2018-08-01 11:51:20 -0400

    Implement dump_steem_schema #683 #2549

--- libraries/chain/include/steem/chain/index.hpp
+++ libraries/chain/include/steem/chain/index.hpp
@@ -1,13 +1,46 @@
 #pragma once
 
+#include <steem/schema/schema.hpp>
+#include <steem/protocol/schema_types.hpp>
+#include <steem/chain/schema_types.hpp>
+
 #include <steem/chain/database.hpp>
 
 namespace steem { namespace chain {
+
+using steem::schema::abstract_schema;
+
+struct index_info
+   : public chainbase::index_extension
+{
+   index_info();
+   virtual ~index_info();
+   virtual std::shared_ptr< abstract_schema > get_schema() = 0;
+};
+
+template< typename MultiIndexType >
+struct index_info_impl
+   : public index_info
+{
+   typedef typename MultiIndexType::value_type value_type;
+
+   index_info_impl()
+      : _schema( steem::schema::get_schema_for_type< value_type >() ) {}
+   virtual ~index_info_impl() {}
+
+   virtual std::shared_ptr< abstract_schema > get_schema() override
+   {   return _schema;   }
+
+   std::shared_ptr< abstract_schema > _schema;
+};
 
 template< typename MultiIndexType >
 void _add_index_impl( database& db )
 {
    db.add_index< MultiIndexType >();
+   std::shared_ptr< chainbase::index_extension > ext =
+      std::make_shared< index_info_impl< MultiIndexType > >();
+   db.add_index_extension< MultiIndexType >( ext );
 }
 
 template< typename MultiIndexType >
