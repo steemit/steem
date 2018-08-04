b4c2be2079d164d865bd6ce621bf50602ab186a5
justinw <juswelch@gmail.com>
2018-08-04 13:49:27 -0400

    docker cleanup needs to happen on slaves

--- ciscripts/buildscript.sh
+++ ciscripts/buildscript.sh
@@ -7,3 +7,5 @@ fi
 sudo docker build --build-arg BUILD_STEP=2 -t=$IMAGE_NAME .
 sudo docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 sudo docker push $IMAGE_NAME
+# make docker cleanup after itself and delete all exited containers
+sudo docker rm -v $(docker ps -a -q -f status=exited) || true
