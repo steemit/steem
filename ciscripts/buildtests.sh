bd0686e104b6d62e9090b4dfc5882d8563b5cf99
justinw <juswelch@gmail.com>
2018-08-04 13:50:26 -0400

    docker cleanup needs to happen on slaves

--- ciscripts/buildtests.sh
+++ ciscripts/buildtests.sh
@@ -2,3 +2,5 @@
 set -e
 sudo docker build --build-arg BUILD_STEP=1 -t=steemit/steem:tests .
 sudo docker run -v $WORKSPACE:/var/jenkins steemit/steem:tests cp -r /var/cobertura /var/jenkins
+# make docker cleanup after itself and delete all exited containers
+sudo docker rm -v $(docker ps -a -q -f status=exited) || true
