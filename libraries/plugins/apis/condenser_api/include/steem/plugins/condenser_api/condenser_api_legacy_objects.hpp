4aeea1181ae2fe8b3a700dd6702dbfa10135d612
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2018-09-10 16:04:11 -0700

    Fix condenser_api extension static_variant #2681

--- libraries/plugins/apis/condenser_api/include/steem/plugins/condenser_api/condenser_api_legacy_objects.hpp
+++ libraries/plugins/apis/condenser_api/include/steem/plugins/condenser_api/condenser_api_legacy_objects.hpp
@@ -9,7 +9,8 @@ typedef static_variant<
          void_t,
          version,
          hardfork_version_vote,
-         automated_actions
+         required_automated_actions,
+         optional_automated_actions
       > legacy_block_header_extensions;
 
 typedef vector< legacy_block_header_extensions > legacy_block_header_extensions_type;
