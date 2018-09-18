8a18908080c1d6860852cb64621c6e492464aa8b
Steve Gerbino <steve@gerbino.co>
2018-09-18 13:22:55 -0400

    Create NAI pool object and index #2886

---
+++ libraries/chain/include/steem/chain/smt_objects/nai_pool_object.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <steem/chain/steem_object_types.hpp>
+#include <steem/protocol/asset_symbol.hpp>
+
+#ifdef STEEM_ENABLE_SMT
+
+namespace steem { namespace chain {
+
+   class nai_pool_object : public object< nai_pool_object_type, nai_pool_object >
+   {
+      nai_pool_object() = delete;
+
+   public:
+      template< typename Constructor, typename Allocator >
+      transaction_object( Constructor&& c, allocator< Allocator > a )
+      {
+         c( *this );
+      }
+
+      id_type              id;
+      asset_symbol_type    asset_symbol;
+   };
+   
+   typedef multi_index_container <
+      nai_pool_object,
+      indexed_by <
+         ordered_unique< tag< by_id >, member< nai_pool_object, nai_pool_id_type, &nai_pool_object::id >
+      >,
+      allocator< nai_pool_object >
+   > smt_token_index;
+
+} } // namespace steem::chain
+
+FC_REFLECT( steem::chain::nai_pool_object, (id)(asset_symbol) )
+
+CHAINBASE_SET_INDEX_TYPE( steem::chain::nai_pool_object, steem::chain::nai_pool_index )
+
+#endif
