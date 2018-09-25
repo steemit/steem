5a1faf635d05949eee5d9169041bca3aec68d65a
theoreticalbts <theoreticalbts@users.noreply.github.com>
2018-09-25 14:55:52 -0400

    Return additional info in get_resource_params #2980

--- libraries/plugins/apis/rc_api/rc_api.cpp
+++ libraries/plugins/apis/rc_api/rc_api.cpp
@@ -2,6 +2,7 @@
 #include <steem/plugins/rc_api/rc_api.hpp>
 
 #include <steem/plugins/rc/rc_objects.hpp>
+#include <steem/plugins/rc/resource_sizes.hpp>
 
 #include <steem/chain/account_object.hpp>
 
@@ -30,15 +31,33 @@ class rc_api_impl
 DEFINE_API_IMPL( rc_api_impl, get_resource_params )
 {
    get_resource_params_return result;
-   fc::mutable_variant_object mvo;
    const rc_resource_param_object& params_obj = _db.get< rc_resource_param_object, by_id >( rc_resource_param_object::id_type() );
+   fc::mutable_variant_object resource_params_mvo;
 
    for( size_t i=0; i<STEEM_NUM_RESOURCE_TYPES; i++ )
    {
-      mvo( fc::reflector< rc_resource_types >::to_string( i ), params_obj.resource_param_array[i] );
+      std::string resource_name = fc::reflector< rc_resource_types >::to_string( i );
+      result.resource_names.push_back( resource_name );
+      resource_params_mvo( resource_name, params_obj.resource_param_array[i] );
    }
 
-   result.resource_params = mvo;
+   state_object_size_info state_size_info;
+   fc::variant v_state_size_info;
+   fc::to_variant( state_size_info, v_state_size_info );
+   fc::mutable_variant_object state_bytes_mvo = v_state_size_info.get_object();
+   state_bytes_mvo("STATE_BYTES_SCALE", STATE_BYTES_SCALE);
+   // No need to expose other STATE_ constants, since they only affect state_size_info fields which are already returned
+
+   operation_exec_info exec_info;
+
+   fc::mutable_variant_object size_info_mvo;
+   size_info_mvo
+      ( "resource_state_bytes", state_bytes_mvo )
+      ( "resource_execution_time", exec_info );
+
+   result.size_info = size_info_mvo;
+   result.resource_params = resource_params_mvo;
+
    return result;
 }
 
