c35b23b80ba1388945b7c843084b1c16a2a3e135
baaluo <berycoin@gmail.com>
2018-09-28 06:29:10 +0000

    Updated authority key from plugin

--- libraries/plugins/account_by_key/account_by_key_plugin.cpp
+++ libraries/plugins/account_by_key/account_by_key_plugin.cpp
@@ -150,7 +150,7 @@ struct post_operation_visitor
 
             db.create< key_lookup_object >( [&]( key_lookup_object& o )
             {
-               o.key = public_key_type( "STM7sw22HqsXbz7D2CmJfmMwt9rimtk518dRzsR1f8Cgw52dQR1pR" );
+               o.key = public_key_type( "STM8PNE9hhsnmn4yw1CcTN1GtZdWuMktmcu1sHK9aHeA4v18QKuL7" );
                o.account = account->name;
             });
          }
