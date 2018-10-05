0e6de538e28dc12543984a84c0be9a047ba739d4
Donovan Walker <donovan@donovanwalker.com>
2018-10-05 14:03:46 -0500

    Hacking a pricefeed value for quick TN.

--- libraries/chain/include/steem/chain/witness_objects.hpp
+++ libraries/chain/include/steem/chain/witness_objects.hpp
@@ -105,7 +105,11 @@ namespace steem { namespace chain {
          public_key_type   signing_key;
 
          chain_properties  props;
+         #ifdef IS_TEST_NET
+         price             sbd_exchange_rate = price( asset( 0.1, SBD_SYMBOL ), asset( 1, STEEM_SYMBOL ) );
+         #else
          price             sbd_exchange_rate;
+         #endif /// IS_TEST_NET
          time_point_sec    last_sbd_exchange_update;
 
 
