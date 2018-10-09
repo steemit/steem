c76361a4f077a5a1fcd82f2ec0849d01aeaf16d3
Steve Gerbino <steve@gerbino.co>
2018-10-09 16:22:25 -0400

    Makes more sense to just decrement first and apply swap #2886

--- libraries/chain/smt_objects/nai_pool.cpp
+++ libraries/chain/smt_objects/nai_pool.cpp
@@ -88,8 +88,8 @@ void remove_from_nai_pool( database &db, const asset_symbol_type& a )
       auto index = std::distance( nais.begin(), it );
       db.modify( npo, [&] ( nai_pool_object& obj )
       {
-         std::swap( obj.nais[ index ], obj.nais[ obj.num_available_nais - 1 ] );
          obj.num_available_nais--;
+         std::swap( obj.nais[ index ], obj.nais[ obj.num_available_nais ] );
       } );
    }
 }
