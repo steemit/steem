7e86e85b6e30ddb6c4b0114c820c3cf4178e5a0d
Steve Gerbino <steve@gerbino.co>
2018-10-10 14:41:09 -0400

    Implemented transaction_status test #2458

--- libraries/plugins/transaction_status/transaction_status_plugin.cpp
+++ libraries/plugins/transaction_status/transaction_status_plugin.cpp
@@ -70,6 +70,9 @@ void transaction_status_plugin::set_program_options( boost::program_options::opt
    cfg.add_options()
       ( TRANSACTION_STATUS_BLOCK_DEPTH_KEY, boost::program_options::value<uint32_t>()->default_value( 64000 ), "Defines the number of blocks from the head block that transaction statuses will be tracked." )
       ;
+   cli.add_options()
+      ( TRANSACTION_STATUS_BLOCK_DEPTH_KEY, boost::program_options::value<uint32_t>()->default_value( 64000 ), "Defines the number of blocks from the head block that transaction statuses will be tracked." )
+      ;
 }
 
 void transaction_status_plugin::plugin_initialize( const boost::program_options::variables_map& options )
--- tests/plugin_tests/transaction_status.cpp
+++ tests/plugin_tests/transaction_status.cpp
@@ -10,6 +10,9 @@
 using namespace steem::chain;
 using namespace steem::protocol;
 
+#define TRANSACTION_STATUS_TEST_BLOCK_DEPTH 10
+#define TRANSACTION_STATUS_TEST_BLOCK_DEPTH_STR BOOST_PP_STRINGIZE( TRANSACTION_STATUS_TEST_BLOCK_DEPTH )
+
 BOOST_FIXTURE_TEST_SUITE( transaction_status, database_fixture )
 
 BOOST_AUTO_TEST_CASE( transaction_status_test )
@@ -20,12 +23,13 @@ BOOST_AUTO_TEST_CASE( transaction_status_test )
    {
       int argc = boost::unit_test::framework::master_test_suite().argc;
       char** argv = boost::unit_test::framework::master_test_suite().argv;
-      for( int i=1; i<argc; i++ )
+
+      for ( int i = 1; i < argc; i++ )
       {
-         const std::string arg = argv[i];
-         if( arg == "--record-assert-trip" )
+         const std::string arg = argv[ i ];
+         if ( arg == "--record-assert-trip" )
             fc::enable_record_assert_trip = true;
-         if( arg == "--show-test-names" )
+         if ( arg == "--show-test-names" )
             std::cout << "running test " << boost::unit_test::framework::current_test_case().p_name << std::endl;
       }
 
@@ -33,10 +37,13 @@ BOOST_AUTO_TEST_CASE( transaction_status_test )
       db_plugin = &appbase::app().register_plugin< steem::plugins::debug_node::debug_node_plugin >();
       init_account_pub_key = init_account_priv_key.get_public_key();
 
+      // We create an argc/argv so that the transaction_status plugin can be initialized with a reasonable block depth
+      int test_argc = 3;
+      const char* test_argv[] = { boost::unit_test::framework::master_test_suite().argv[0], "--transaction-status-block-depth", TRANSACTION_STATUS_TEST_BLOCK_DEPTH_STR };
       db_plugin->logging = false;
       appbase::app().initialize<
          steem::plugins::transaction_status::transaction_status_plugin,
-         steem::plugins::debug_node::debug_node_plugin >( argc, argv );
+         steem::plugins::debug_node::debug_node_plugin >( test_argc, (char**)test_argv );
 
       db = &appbase::app().get_plugin< steem::plugins::chain::chain_plugin >().db();
       BOOST_REQUIRE( db );
@@ -68,34 +75,67 @@ BOOST_AUTO_TEST_CASE( transaction_status_test )
 
       generate_block();
 
-      //const auto& tx_status_idx = db->get_index< transaction_status_index >().indices().get< by_trx_id >();
-
       validate_database();
 
-      signed_transaction tx;
-      transfer_operation op;
+      signed_transaction tx1;
+      transfer_operation op1;
 
-      op.from = "alice";
-      op.to = "bob";
-      op.amount = ASSET( "5.000 TESTS" );
+      op1.from = "alice";
+      op1.to = "bob";
+      op1.amount = ASSET( "5.000 TESTS" );
 
-      BOOST_TEST_MESSAGE( "--- Test normal transaction" );
-      tx.operations.push_back( op );
-      tx.set_expiration( db->head_block_time() + STEEM_MAX_TIME_UNTIL_EXPIRATION );
-      sign( tx, alice_private_key );
-      db->push_transaction( tx, 0 );
+      // Create transaction 1
+      tx1.operations.push_back( op1 );
+      tx1.set_expiration( db->head_block_time() + STEEM_MAX_TIME_UNTIL_EXPIRATION );
+      sign( tx1, alice_private_key );
+      db->push_transaction( tx1, 0 );
 
-      auto tso = db->find< transaction_status_object, by_trx_id >( tx.id() );
+      // Transaction 1 exists in the mem pool
+      auto tso = db->find< transaction_status_object, by_trx_id >( tx1.id() );
       BOOST_REQUIRE( tso != nullptr );
       BOOST_REQUIRE( tso->block_num == 0 );
 
       generate_block();
 
-      tso = db->find< transaction_status_object, by_trx_id >( tx.id() );
+      signed_transaction tx2;
+      transfer_operation op2;
+
+      op2.from = "alice";
+      op2.to = "bob";
+      op2.amount = ASSET( "5.000 TESTS" );
+
+      // Create transaction 2
+      tx2.operations.push_back( op2 );
+      tx2.set_expiration( db->head_block_time() + STEEM_MAX_TIME_UNTIL_EXPIRATION );
+      sign( tx2, alice_private_key );
+      db->push_transaction( tx2, 0 );
+
+      // Transaction 1 exists in a block
+      tso = db->find< transaction_status_object, by_trx_id >( tx1.id() );
       BOOST_REQUIRE( tso != nullptr );
       BOOST_REQUIRE( tso->block_num == db->head_block_num() );
 
+      // Transaction 2 exists in a mem pool
+      tso = db->find< transaction_status_object, by_trx_id >( tx2.id() );
+      BOOST_REQUIRE( tso != nullptr );
+      BOOST_REQUIRE( tso->block_num == 0 );
 
+      generate_blocks( TRANSACTION_STATUS_TEST_BLOCK_DEPTH );
+
+      // Transaction 1 is too old to be tracked
+      tso = db->find< transaction_status_object, by_trx_id >( tx1.id() );
+      BOOST_REQUIRE( tso == nullptr );
+
+      // Transaction 2 exists in a block
+      tso = db->find< transaction_status_object, by_trx_id >( tx2.id() );
+      BOOST_REQUIRE( tso != nullptr );
+      BOOST_REQUIRE( tso->block_num > 0 );
+
+      generate_block();
+
+      // Transaction 2 is too old to be tracked
+      tso = db->find< transaction_status_object, by_trx_id >( tx2.id() );
+      BOOST_REQUIRE( tso == nullptr );
    }
    FC_LOG_AND_RETHROW()
 }
