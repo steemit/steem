90ff402ab7595c8e924cbdaf9355964575aa2572
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2018-10-25 16:28:56 -0700

    Allow scheduling future automated actions #2718

--- libraries/chain/include/steem/chain/pending_required_action_object.hpp
+++ libraries/chain/include/steem/chain/pending_required_action_object.hpp
@@ -20,15 +20,24 @@ class pending_required_action_object : public object< pending_required_action_ob
          c( *this );
       }
 
-      id_type           id;
+      id_type                    id;
 
+      time_point_sec             execution_time;
       required_automated_action  action;
 };
+
+struct by_execution;
 
 typedef multi_index_container<
    pending_required_action_object,
    indexed_by<
-      ordered_unique< tag< by_id >, member< pending_required_action_object, pending_required_action_id_type, &pending_required_action_object::id > >
+      ordered_unique< tag< by_id >, member< pending_required_action_object, pending_required_action_id_type, &pending_required_action_object::id > >,
+      ordered_unique< tag< by_execution >,
+         composite_key< pending_required_action_object,
+            member< pending_required_action_object, time_point_sec, &pending_required_action_object::execution_time >,
+            member< pending_required_action_object, pending_required_action_id_type, &pending_required_action_object::id >
+         >
+      >
    >,
    allocator< pending_required_action_object >
 > pending_required_action_index;
@@ -36,5 +45,5 @@ typedef multi_index_container<
 } } //steem::chain
 
 FC_REFLECT( steem::chain::pending_required_action_object,
-            (id)(action) )
+            (id)(execution_time)(action) )
 CHAINBASE_SET_INDEX_TYPE( steem::chain::pending_required_action_object, steem::chain::pending_required_action_index )
