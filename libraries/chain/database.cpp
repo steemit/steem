bedfd5d7624f9f904aeee7928e442a021b0feaec
theoreticalbts <theoreticalbts@users.noreply.github.com>
2018-10-27 17:53:05 -0400

    Move core indexes into own file #3118

--- libraries/chain/database.cpp
+++ libraries/chain/database.cpp
@@ -1,3 +1,4 @@
+
 #include <steem/protocol/steem_operations.hpp>
 
 #include <steem/chain/block_summary_object.hpp>
@@ -9,7 +10,6 @@
 #include <steem/chain/evaluator_registry.hpp>
 #include <steem/chain/global_property_object.hpp>
 #include <steem/chain/history_object.hpp>
-#include <steem/chain/index.hpp>
 #include <steem/chain/pending_required_action_object.hpp>
 #include <steem/chain/pending_optional_action_object.hpp>
 #include <steem/chain/smt_objects.hpp>
@@ -2580,46 +2580,11 @@ std::shared_ptr< custom_operation_interpreter > database::get_custom_json_evalua
    return std::shared_ptr< custom_operation_interpreter >();
 }
 
+void initialize_core_indexes( database& db );
+
 void database::initialize_indexes()
 {
-   add_core_index< dynamic_global_property_index           >(*this);
-   add_core_index< account_index                           >(*this);
-   add_core_index< account_authority_index                 >(*this);
-   add_core_index< witness_index                           >(*this);
-   add_core_index< transaction_index                       >(*this);
-   add_core_index< block_summary_index                     >(*this);
-   add_core_index< witness_schedule_index                  >(*this);
-   add_core_index< comment_index                           >(*this);
-   add_core_index< comment_content_index                   >(*this);
-   add_core_index< comment_vote_index                      >(*this);
-   add_core_index< witness_vote_index                      >(*this);
-   add_core_index< limit_order_index                       >(*this);
-   add_core_index< feed_history_index                      >(*this);
-   add_core_index< convert_request_index                   >(*this);
-   add_core_index< liquidity_reward_balance_index          >(*this);
-   add_core_index< operation_index                         >(*this);
-   add_core_index< account_history_index                   >(*this);
-   add_core_index< hardfork_property_index                 >(*this);
-   add_core_index< withdraw_vesting_route_index            >(*this);
-   add_core_index< owner_authority_history_index           >(*this);
-   add_core_index< account_recovery_request_index          >(*this);
-   add_core_index< change_recovery_account_request_index   >(*this);
-   add_core_index< escrow_index                            >(*this);
-   add_core_index< savings_withdraw_index                  >(*this);
-   add_core_index< decline_voting_rights_request_index     >(*this);
-   add_core_index< reward_fund_index                       >(*this);
-   add_core_index< vesting_delegation_index                >(*this);
-   add_core_index< vesting_delegation_expiration_index     >(*this);
-   add_core_index< pending_required_action_index           >(*this);
-   add_core_index< pending_optional_action_index           >(*this);
-#ifdef STEEM_ENABLE_SMT
-   add_core_index< smt_token_index                         >(*this);
-   add_core_index< smt_event_token_index                   >(*this);
-   add_core_index< account_regular_balance_index           >(*this);
-   add_core_index< account_rewards_balance_index           >(*this);
-   add_core_index< nai_pool_index                          >(*this);
-#endif
-
+   initialize_core_indexes( *this );
    _plugin_index_signal();
 }
 
@@ -5442,8 +5407,5 @@ optional< chainbase::database::session >& database::pending_transaction_session(
 {
    return _pending_tx_session;
 }
-
-index_info::index_info() {}
-index_info::~index_info() {}
 
 } } //steem::chain
