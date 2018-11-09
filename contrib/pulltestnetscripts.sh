1a9adca6f2bc5ef955bf1d9986f8d9068b87b91e
Justin Welch <juswelch@gmail.com>
2018-11-09 14:09:20 -0500

    overwrite the healtcheck nginx conf for testnet

--- contrib/pulltestnetscripts.sh
+++ contrib/pulltestnetscripts.sh
@@ -5,7 +5,7 @@ echo steemd-testnet: getting deployment scripts from external source
 wget -qO- $SCRIPTURL/master/$LAUNCHENV/$APP/testnetinit.sh > /usr/local/bin/testnetinit.sh
 wget -qO- $SCRIPTURL/master/$LAUNCHENV/$APP/testnet.config.ini > /etc/steemd/testnet.config.ini
 wget -qO- $SCRIPTURL/master/$LAUNCHENV/$APP/fastgen.config.ini > /etc/steemd/fastgen.config.ini
-wget -qO- $SCRIPTURL/master/$LAUNCHENV/$APP/testnet-web-console.conf > /etc/nginx/sites-enabled/testnet-web-console.conf
+wget -qO- $SCRIPTURL/master/$LAUNCHENV/$APP/testnet-web-console.conf > /etc/nginx/healthcheck.conf.template
 chmod +x /usr/local/bin/testnetinit.sh
 
 echo steemd-testnet: launching testnetinit script
