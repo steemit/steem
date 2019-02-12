e359fa584d5dcefbf1660565861d47d51a2dae8b
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2019-02-12 09:36:31 -0800

    Bump cache to 40 million

--- libraries/chain/database.cpp
+++ libraries/chain/database.cpp
@@ -3153,7 +3153,7 @@ void database::_apply_block( const signed_block& next_block )
    // last call of applying a block because it is the only thing that is not
    // reversible.
    migrate_irreversible_state();
-   trim_cache( 2000000 );
+   trim_cache( 40000000 );
 } FC_CAPTURE_LOG_AND_RETHROW( (next_block.block_num()) ) }
 
 struct process_header_visitor
