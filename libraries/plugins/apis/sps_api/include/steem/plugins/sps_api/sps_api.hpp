9df3d363b9796e0b02e50b4c75c68ad2068d30da
Dariusz Kedzierski <dkedzierski@syncad.com>
2019-02-19 15:48:19 +0100

    Added body to RPC API methods. Code cleanup: usage of sps objects in api.

--- libraries/plugins/apis/sps_api/include/steem/plugins/sps_api/sps_api.hpp
+++ libraries/plugins/apis/sps_api/include/steem/plugins/sps_api/sps_api.hpp
@@ -1,15 +1,12 @@
 #pragma once
 #include <steem/plugins/json_rpc/utility.hpp>
-
-#include <steem/protocol/types.hpp>
-#include <steem/chain/steem_object_types.hpp>
-#include <steem/protocol/asset.hpp>
+#include <steem/chain/sps_objects.hpp>
 
 namespace steem { namespace plugins { namespace sps {
   using plugins::json_rpc::void_type;
   using steem::chain::account_name_type;
-  using fc::time_point_sec;
-  using steem::protocol::asset;
+  using steem::chain::proposal_object;
+  using steem::chain::proposal_id_type;
 
   namespace detail
   {
@@ -22,47 +19,15 @@ namespace steem { namespace plugins { namespace sps {
     direction_descending
   };
 
-  typedef uint64_t id_type;
-
-  struct proposal_object 
-  {
-    //internal key
-    id_type id;
-
-    // account that created the proposal
-    account_name_type creator;
-
-    // account_being_funded
-    account_name_type receiver;
-
-    // start_date (when the proposal will begin paying out if it gets enough vote weight)
-    time_point_sec start_date;
-
-    // end_date (when the proposal expires and can no longer pay out)
-    time_point_sec end_date;
-
-    //daily_pay (the amount of SBD that is being requested to be paid out daily)
-    asset daily_pay;
-
-    //subject (a very brief description or title for the proposal)
-    string subject;
-
-    //url (a link to a page describing the work proposal in depth, generally this will probably be to a Steem post).
-    string url;
-
-    //This will be calculate every maintenance period
-    uint64_t total_votes = 0;
-  };
-
   // Struct with arguments for find_proposal methd
   struct find_proposal_args 
   {
     // id of the proposal to find
-    id_type id;
+    steem::chain::proposal_id_type id;
   };
 
   // Return type for find_proposal method
-  typedef proposal_object find_proposal_return;
+  typedef std::vector<proposal_object> find_proposal_return;
   
   // Struct with argumentse for list_proposals method
   struct list_proposals_args 
@@ -115,18 +80,6 @@ namespace steem { namespace plugins { namespace sps {
 FC_REFLECT_ENUM(steem::plugins::sps::order_direction_type, 
   (direction_ascending)
   (direction_descending)
-  );
-
-FC_REFLECT(steem::plugins::sps::proposal_object, 
-  (id)
-  (creator)
-  (receiver)
-  (start_date)
-  (end_date)
-  (daily_pay)
-  (subject)
-  (url)
-  (total_votes) 
   );
 
 FC_REFLECT(steem::plugins::sps::find_proposal_args, 
