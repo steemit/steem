eb55b8415735f1850d2cb325f0d3c89a42f236ff
Bartek Wrona <wrona@syncad.com>
2019-02-22 17:33:53 +0100

    [BW]: [NIP] Issue #7 Implemented vop: `proposal_pay_operation` and applied to get_impacted_account_visitor to have correctly recorded AH. To be tested.

--- libraries/protocol/include/steem/protocol/operations.hpp
+++ libraries/protocol/include/steem/protocol/operations.hpp
@@ -79,6 +79,10 @@ namespace steem { namespace protocol {
             smt_set_runtime_parameters_operation,
             smt_create_operation,
 #endif
+            create_proposal_operation,
+            update_proposal_votes_operation,
+            remove_proposal_operation,
+
             /// virtual operations below this point
             fill_convert_request_operation,
             author_reward_operation,
@@ -96,9 +100,7 @@ namespace steem { namespace protocol {
             comment_benefactor_reward_operation,
             producer_reward_operation,
             clear_null_account_balance_operation,
-            create_proposal_operation,
-            update_proposal_votes_operation,
-            remove_proposal_operation
+            proposal_pay_operation
          > operation;
 
    /*void operation_get_required_authorities( const operation& op,
