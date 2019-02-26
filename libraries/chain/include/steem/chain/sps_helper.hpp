9e00f0949595dff64d8206378615aaafe2afe501
mtrela <mtrela@syncad.com>
2019-02-26 11:36:00 +0100

    Simplifaction of cleanup algorithm

--- libraries/chain/include/steem/chain/sps_helper.hpp
+++ libraries/chain/include/steem/chain/sps_helper.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <steem/chain/database.hpp>
+#include <steem/chain/sps_objects.hpp>
+
 #include <boost/container/flat_set.hpp>
 
 namespace steem { namespace chain {
@@ -9,15 +11,30 @@ using boost::container::flat_set;
 
 class sps_helper
 {
-   private:
-
-      template< bool IS_CHECK >
-      static void remove_proposals_impl( database& db, const flat_set<int64_t>& proposal_ids, const account_name_type& proposal_owner );
-
    public:
 
+      template<   typename ByProposalType,
+                  typename ProposalObjectIterator,
+                  typename ProposalIndex, typename VotesIndex, typename ByVoterIdx >
+      static ProposalObjectIterator remove_proposal( const ProposalObjectIterator& proposal,
+                  ProposalIndex& proposalIndex, VotesIndex& votesIndex, const ByVoterIdx& byVoterIdx )
+      {
+         ilog("Erasing all votes associated to proposal: ${p}", ("p", *proposal));
+
+         /// Now remove all votes specific to given proposal.
+         auto propI = byVoterIdx.lower_bound(boost::make_tuple(proposal->id, account_name_type()));
+
+         while(propI != byVoterIdx.end() && static_cast< size_t >( propI->id ) == static_cast< size_t >( proposal->id ) )
+         {
+            propI = votesIndex. template erase<by_proposal_voter>(propI);
+         }
+
+         ilog("Erasing proposal: ${p}", ("p", *proposal));
+
+         return proposalIndex. template erase< ByProposalType >( proposal );
+      }
+
       static void remove_proposals( database& db, const flat_set<int64_t>& proposal_ids, const account_name_type& proposal_owner );
-      static void remove_proposals( database& db, const flat_set<int64_t>& proposal_ids );
 };
 
 } } // steem::chain
--- libraries/chain/sps_helper.cpp
+++ libraries/chain/sps_helper.cpp
@@ -1,16 +1,14 @@
 #include <steem/chain/sps_helper.hpp>
-#include <steem/chain/sps_objects.hpp>
 
 namespace steem { namespace chain {
 
-template< bool IS_CHECK >
-void sps_helper::remove_proposals_impl( database& db, const flat_set<int64_t>& proposal_ids, const account_name_type& proposal_owner )
+void sps_helper::remove_proposals( database& db, const flat_set<int64_t>& proposal_ids, const account_name_type& proposal_owner )
 {
    if( proposal_ids.empty() )
       return;
 
-   auto& proposalIdx = db.get_mutable_index< proposal_index >();
-   auto& byIdIdx = proposalIdx.indices().get< by_id >();
+   auto& proposalIndex = db.get_mutable_index< proposal_index >();
+   auto& byIdIdx = proposalIndex.indices().get< by_id >();
 
    auto& votesIndex = db.get_mutable_index< proposal_vote_index >();
    auto& byVoterIdx = votesIndex.indices().get< by_proposal_voter >();
@@ -22,36 +20,11 @@ void sps_helper::remove_proposals_impl( database& db, const flat_set<int64_t>& p
       if(foundPosI == byIdIdx.end())
          continue;
 
-      const auto& proposal = *foundPosI;
+      FC_ASSERT(foundPosI->creator == proposal_owner, "Only proposal owner can remove it...");
 
-      if( IS_CHECK )
-         FC_ASSERT(proposal.creator == proposal_owner, "Only proposal owner can remove it...");
-
-      ilog("Erasing all votes associated to proposal: ${p}", ("p", proposal));
-
-      /// Now remove all votes specific to given proposal.
-      auto propI = byVoterIdx.lower_bound(boost::make_tuple(proposal.id, account_name_type()));
-
-      while(propI != byVoterIdx.end() && propI->id == proposal.id)
-      {
-         propI = votesIndex.erase<by_proposal_voter>(propI);
-      }
-
-      ilog("Erasing proposal: ${p}", ("p", proposal));
-
-      proposalIdx.remove(proposal);
+      remove_proposal< by_id >( foundPosI, proposalIndex, votesIndex, byVoterIdx );
    }
 
-}
-
-void sps_helper::remove_proposals( database& db, const flat_set<int64_t>& proposal_ids, const account_name_type& proposal_owner )
-{
-   remove_proposals_impl< true/*IS_CHECK*/ >( db, proposal_ids, proposal_owner );
-}
-
-void sps_helper::remove_proposals( database& db, const flat_set<int64_t>& proposal_ids )
-{
-   remove_proposals_impl< false/*IS_CHECK*/ >( db, proposal_ids, account_name_type() );
 }
 
 } } // steem::chain
