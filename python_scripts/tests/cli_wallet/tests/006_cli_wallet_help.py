215219b34f65c2874a7c159a17dcad874552fc1b
Wieslaw <wkedzierski@syncad.com>
2019-03-08 14:57:45 +0100

    Add cli_wallet args test.

---
+++ python_scripts/tests/cli_wallet/tests/006_cli_wallet_help.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import subprocess
+
+from utils.test_utils import *
+from utils.cmd_args   import args
+from utils.logger     import log
+
+if __name__ == "__main__":
+    try:
+        error = False
+        output = subprocess.run([args.path+"/cli_wallet", "--help"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output_stdout = output.stdout.decode('utf-8')
+        args_founded = [ arg for arg in output_stdout.split() if "--" in arg ]
+        only_args_to_be_founded = ['--help', '--server-rpc-endpoint', '--cert-authority', '--rpc-endpoint', '--rpc-tls-endpoint', '--rpc-tls-certificate', '--rpc-http-endpoint', '--daemon', '--rpc-http-allowip', '--wallet-file', '--chain-id']
+        res = list(set(args_founded)^set(only_args_to_be_founded))
+        if res:
+            raise ArgsCheckException("There are some additional argument in cli_wallet `{0}`.".format(res))
+
+    except Exception as _ex:
+        log.exception(str(_ex))
+        error = True
+    finally:
+        if error:
+            log.error("TEST `{0}` failed".format(__file__))
+            exit(1)
+        else:
+            log.info("TEST `{0}` passed".format(__file__))
+            exit(0)
+
+
+
