cabbb00df461307238f6766f9562bd5eaa3ef1d5
Wiesław Kędzierski <w.m.kedzierski@gmail.com>
2019-03-14 17:36:54 +0100

    Wk issue #85 inactive proposals removal (#89)
    * Inactive proposals will no longer be removed - need to add automatic actions.
    
    * Remove unnecessary code.
    
    * Removal proposal unitest update.
    
    * Leave remove_proposal code but comment out call to it.
    
    * Remove proposal unitest disabled.

--- tests/plugin_tests/proposal.cpp
+++ tests/plugin_tests/proposal.cpp
@@ -93,11 +93,13 @@ BOOST_AUTO_TEST_CASE( generating_payments )
    FC_LOG_AND_RETHROW()
 }
 
-BOOST_AUTO_TEST_CASE( proposals_maintenance )
+BOOST_AUTO_TEST_CASE( proposals_maintenance, * boost::unit_test::disabled())
 {
    try
    {
       BOOST_TEST_MESSAGE( "Testing: removing inactive proposals" );
+      //Update see issue #85 -> https://github.com/blocktradesdevs/steem/issues/85
+      //Remove proposal will be automatic action - this test shall be temporary disabled.
 
       plugin_prepare();
 
