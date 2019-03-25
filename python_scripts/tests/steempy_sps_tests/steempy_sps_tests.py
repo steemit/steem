62300a51d668f05be260094dbd7b7e1a5c0192b7
Dariusz Kedzierski <dkedzierski@syncad.com>
2019-03-25 15:23:53 +0100

    Test for ordered iteration

--- python_scripts/tests/steempy_sps_tests/steempy_sps_tests.py
+++ python_scripts/tests/steempy_sps_tests/steempy_sps_tests.py
@@ -31,6 +31,22 @@ except Exception as ex:
     logger.error("SteemPy library is not installed.")
     sys.exit(1)
 
+def get_date_as_isostr(date):
+    return date.replace(microsecond=0).isoformat()
+
+
+def get_isostr_start_end_date(now, start_date_delta, end_date_delta):
+    from datetime import timedelta
+
+    start_date = now + timedelta(days = start_date_delta)
+    end_date = start_date + timedelta(days = end_date_delta)
+
+    start_date = start_date.replace(microsecond=0).isoformat()
+    end_date = end_date.replace(microsecond=0).isoformat()
+
+    return start_date, end_date
+
+
 def test_create_proposal(node, creator_account, receiver_account, wif, subject):
     logger.info("Testing: create_proposal")
     s = Steem(nodes = [node], no_broadcast = False, keys = [wif])
@@ -38,11 +54,7 @@ def test_create_proposal(node, creator_account, receiver_account, wif, subject):
     import datetime
     now = datetime.datetime.now()
 
-    start_date = now + datetime.timedelta(days = 1)
-    end_date = start_date + datetime.timedelta(days = 2)
-
-    start_date = start_date.replace(microsecond=0).isoformat()
-    end_date = end_date.replace(microsecond=0).isoformat()
+    start_date, end_date = get_isostr_start_end_date(now, 10, 2)
 
     from steem.account import Account
     try:
@@ -182,9 +194,109 @@ def test_remove_proposal(node, account, wif, subject):
     found = None
     for proposal in proposals:
         if proposal["subject"] == subject:
-          found = proposal
+            found = proposal
     
     assert found is None
+
+
+def test_iterate_results_test(node, creator_account, receiver_account, wif, subject, remove):
+    logger.info("Testing: test_iterate_results_test")
+    # test for iterate prosals
+    # 1 we will create n proposals of which k proposal will have the same value in one of the fields
+    # 2 then we will list proposals starting from kth proposal with limit set to m < k
+    # 3 we list proposals again with the same conditiona as in 2, we should get the same set of results
+    #   in real life scenatio pagination scheme with limit set to value lower than "k" will be showing
+    #   the same results and will hang
+    # 4 then we will use newly introduced last_id field, we should see diferent set of proposals
+    s = Steem(nodes = [node], no_broadcast = False, keys = [wif])
+
+    from steem.account import Account
+    try:
+        creator = Account(creator_account)
+    except Exception as ex:
+        logger.error("Account: {} not found. {}".format(creator_account, ex))
+        sys.exit(1)
+    
+    try:
+        receiver = Account(receiver_account)
+    except Exception as ex:
+        logger.error("Account: {} not found. {}".format(receiver_account, ex))
+        sys.exit(1)
+
+    import datetime
+    now = datetime.datetime.now()
+
+    # 1 we will create n proposals of which k proposal will have the same value in one of the fields
+    # here we have 5 proposals with the same start date
+    start_end_pairs = [
+        [1,1],
+        [2,2],
+        [4,3],
+        [5,4],
+        [5,5],
+        [5,6],
+        [5,7],
+        [5,8],
+        [6,9]
+    ]
+
+    for start_end_pair in start_end_pairs:
+        start_date, end_date = get_isostr_start_end_date(now, start_end_pair[0], start_end_pair[1])
+
+        s.commit.create_proposal(
+            creator["name"], 
+            receiver["name"], 
+            start_date, 
+            end_date,
+            "16.000 TBD",
+            subject,
+            "steempy-proposal-title"
+            )
+        sleep(3)
+    sleep(6)
+
+    start_date = get_date_as_isostr(now + datetime.timedelta(days = 5))
+
+    # 2 then we will list proposals starting from kth proposal with limit set to m < k
+    proposals = s.list_proposals(start_date, "by_start_date", "direction_descending", 3, "all")
+    assert len(proposals) == 3, "Expected {} elements got {}".format(3, len(proposals))
+    ids = []
+    for proposal in proposals:
+        assert proposal["start_date"] == start_date, "Expected start_date do not match {} != {}".format(start_date, proposals[-1]["start_date"])
+        ids.append(proposal["id"])
+    assert len(ids) == 3, "Expected {} elements got {}".format(3, len(ids))
+
+    # 3 we list proposals again with the same conditiona as in 2, we should get the same set of results
+    proposals = s.list_proposals(start_date, "by_start_date", "direction_descending", 3, "all")
+    assert len(proposals) == 3, "Expected {} elements got {}".format(3, len(proposals))
+    oids = []
+    for proposal in proposals:
+        assert proposal["start_date"] == start_date, "Expected start_date do not match {} != {}".format(start_date, proposals[-1]["start_date"])
+        oids.append(proposal["id"])
+    assert len(oids) == 3, "Expected {} elements got {}".format(3, len(oids))
+
+    # the same set of results check
+    for id in ids:
+        assert id in oids, "Id not found in expected results array {}".format(id)
+
+    # 4 then we will use newly introduced last_id field, we should see diferent set of proposals
+    proposals = s.list_proposals(start_date, "by_start_date", "direction_descending", 3, "all", oids[-1])
+
+    start_date, end_date = get_isostr_start_end_date(now, 5, 4)
+
+    assert proposals[-1]["start_date"] == start_date, "Expected start_date do not match {} != {}".format(start_date, proposals[-1]["start_date"])
+    assert proposals[-1]["end_date"] == end_date, "Expected end_date do not match {} != {}".format(end_date, proposals[-1]["end_date"])
+
+    # remove all created proposals
+    if remove:
+        start_date = get_date_as_isostr(now + datetime.timedelta(days = 6))
+        for a in range(0, 2):
+            proposals = s.list_proposals(start_date, "by_start_date", "direction_descending", 5, "all")
+            ids = []
+            for proposal in proposals:
+                ids.append(int(proposal['id']))
+            s.commit.remove_proposal(creator["name"], ids)
+            sleep(12)
 
 
 if __name__ == '__main__':
@@ -213,3 +325,6 @@ if __name__ == '__main__':
     sleep(6)
     if args.no_erase_proposal:
         test_remove_proposal(args.node_url, args.creator, args.wif, subject)
+    sleep(6)
+    test_iterate_results_test(args.node_url, args.creator, args.receiver, args.wif, str(uuid4()), args.no_erase_proposal)
+    sleep(6)
