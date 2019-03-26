08a6c43b3cd0bb633b3dd534bce71cb675794059
Steve Gerbino <steve@gerbino.co>
2019-03-26 19:45:00 -0400

    Fix new clang compiler warnings

--- libraries/fc/tests/crypto/bigint_test.cpp
+++ libraries/fc/tests/crypto/bigint_test.cpp
@@ -46,17 +46,17 @@ BOOST_AUTO_TEST_CASE(bigint_test_2)
         BOOST_CHECK( bi_accu >= a_1 );
     } while ( bi_accu.log2() <= 128 );
 
-    bi_accu = bi_accu;
+    fc::bigint bi_accu2 = bi_accu;
 
-    BOOST_CHECK( bi_accu && !bi_accu.is_negative() && bi_accu != bi_1 );
+    BOOST_CHECK( bi_accu2 && !bi_accu2.is_negative() && bi_accu2 != bi_1 );
 
-    BOOST_CHECK( bi_3.exp( bi_accu.log2() ) > bi_accu );
+    BOOST_CHECK( bi_3.exp( bi_accu2.log2() ) > bi_accu );
 
     fc::bigint big(1);
     big <<= 30; big += bi_17; big <<= 30; big++;
     big <<= 30; big -= bi_17; big >>= 5; big--;
-    fc::bigint rest = bi_accu % big;
-    BOOST_CHECK( (bi_accu - rest) / big == bi_accu / big );
+    fc::bigint rest = bi_accu2 % big;
+    BOOST_CHECK( (bi_accu2 - rest) / big == bi_accu2 / big );
 
     fc::bigint big2; big2 = big;
     big2 *= bi_65537.exp(3);
@@ -69,7 +69,7 @@ BOOST_AUTO_TEST_CASE(bigint_test_2)
     BOOST_CHECK_EQUAL( (std::string) bi_17, "17" );
     BOOST_CHECK_EQUAL( (std::string) bi_65537, "65537" );
     BOOST_CHECK_EQUAL( (std::string) bi_65537.exp(3), "281487861809153" );
-    BOOST_CHECK_EQUAL( (std::string) bi_accu, "12864938683278671740537145998360961546653259485195806" );
+    BOOST_CHECK_EQUAL( (std::string) bi_accu2, "12864938683278671740537145998360961546653259485195806" );
     BOOST_CHECK_EQUAL( (std::string) big, "38685626840157682946539517" );
 }
 
