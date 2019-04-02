f3b6f83f9cf7b5a271207fedffe2bc825df959c4
Steve Gerbino <steve@gerbino.co>
2019-04-02 12:44:15 -0400

    Avoid calls to fc::thread::current(), possible leak

--- libraries/fc/src/log/log_message.cpp
+++ libraries/fc/src/log/log_message.cpp
@@ -52,8 +52,7 @@ namespace fc
       my->line        = line;
       my->method      = method;
       my->timestamp   = time_point::now();
-      const char* current_task_desc = fc::thread::current().current_task_desc();
-      my->task_name   = current_task_desc ? current_task_desc : "?unnamed?";
+      my->task_name   = "";
    }
 
    log_context::log_context( const variant& v )
