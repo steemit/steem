985ed9011de8feb4fbf114cf4c6ebc5802aae09e
Steve Gerbino <steve@gerbino.co>
2019-06-19 14:15:33 -0400

    #2730: Improve error handling in SMT contribution operation validation

--- libraries/protocol/smt_operations.cpp
+++ libraries/protocol/smt_operations.cpp
@@ -386,8 +386,8 @@ void smt_contribute_operation::validate() const
 {
    validate_account_name( contributor );
    common_symbol_validation( symbol );
-   FC_ASSERT( contribution.symbol == STEEM_SYMBOL );
-   FC_ASSERT( contribution.amount > 0 );
+   FC_ASSERT( contribution.symbol == STEEM_SYMBOL, "Contributions must be made in STEEM" );
+   FC_ASSERT( contribution.amount > 0, "Contribution amount must be greater than 0" );
 }
 
 } }
