1c7c4054ac0b4c4a65adb489d2f0ab5bb4c8f8c6
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2019-07-24 15:59:59 -0700

    Update remote_node_api for condenser_api changes #3422

--- libraries/wallet/remote_node_api.cpp
+++ libraries/wallet/remote_node_api.cpp
@@ -429,12 +429,12 @@ flat_set< uint32_t > remote_node_api::get_market_history_buckets()
    FC_ASSERT( false );
 }
 
-vector< database_api::api_proposal_object > remote_node_api::list_proposals( fc::variant, uint32_t, database_api::sort_order_type, database_api::order_direction_type, database_api::proposal_status )
+vector< condenser_api::api_proposal_object > remote_node_api::list_proposals( fc::variant, uint32_t, database_api::sort_order_type, database_api::order_direction_type, database_api::proposal_status )
 {
    FC_ASSERT( false );
 }
 
-vector< database_api::api_proposal_object > remote_node_api::find_proposals( vector< int64_t > )
+vector< condenser_api::api_proposal_object > remote_node_api::find_proposals( vector< int64_t > )
 {
    FC_ASSERT( false );
 }
