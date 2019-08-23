026710d42d5f9622fe4c6792767bc40193490f32
Steve Gerbino <steve@gerbino.co>
2019-08-23 15:19:03 -0400

    #3461: Move example required example to the end of the static variant

--- libraries/protocol/include/steem/protocol/required_automated_actions.hpp
+++ libraries/protocol/include/steem/protocol/required_automated_actions.hpp
@@ -9,23 +9,19 @@ namespace steem { namespace protocol {
    /** NOTE: do not change the order of any actions or it will trigger a hardfork.
     */
    typedef fc::static_variant<
-#ifdef IS_TEST_NET
-            example_required_action
-            ,
-#endif
             smt_ico_launch_action,
             smt_ico_evaluation_action,
             smt_token_launch_action,
             smt_refund_action,
             smt_contributor_payout_action,
             smt_founder_payout_action
+#ifdef IS_TEST_NET
+            ,example_required_action
+#endif
          > required_automated_action;
 
 } } // steem::protocol
 
 STEEM_DECLARE_OPERATION_TYPE( steem::protocol::required_automated_action );
 
-//FC_TODO( "Remove ifdef when first required automated action is added" )
-//#ifdef IS_TEST_NET
 FC_REFLECT_TYPENAME( steem::protocol::required_automated_action );
-//#endif
