90b0d7e1cac23e4f8dac072f7da51b907a598317
Matt <yrp1990@gmail.com>
2020-04-01 19:24:08 +0800

    add some seednode

--- libraries/plugins/p2p/include/steem/plugins/p2p/p2p_default_seeds.hpp
+++ libraries/plugins/p2p/include/steem/plugins/p2p/p2p_default_seeds.hpp
@@ -8,28 +8,18 @@ namespace steem{ namespace plugins { namespace p2p {
 const std::vector< std::string > default_seeds;
 #else
 const std::vector< std::string > default_seeds = {
-   "seed-east.steemit.com:2001",          // steemit
-   "seed-central.steemit.com:2001",       // steemit
-   "seed-west.steemit.com:2001",          // steemit
-   "52.74.152.79:2001",                   // smooth.witness
-   "anyx.io:2001",                        // anyx
-   "seed.liondani.com:2016",              // liondani
-   "gtg.steem.house:2001",                // gtg
-   "seed.jesta.us:2001",                  // jesta
-   "lafonasteem.com:2001",                // lafona
-   "steem-seed.altcap.io:40696",          // ihashfury
-   "seed.roelandp.nl:2001",               // roelandp
-   "seed.timcliff.com:2001",              // timcliff
-   "steemseed.clayop.com:2001",           // clayop
-   "seed.steemviz.com:2001",              // ausbitbank
-   "steem-seed.lukestokes.info:2001",     // lukestokes
-   "seed.steemian.info:2001",             // drakos
-   "seed.followbtcnews.com:2001",         // followbtcnews
-   "node.mahdiyari.info:2001",            // mahdiyari
-   "seed.riversteem.com:2001",            // riverhead
-   "seed1.blockbrothers.io:2001",         // blockbrothers
-   "steemseed-fin.privex.io:2001",        // privex
-   "seed.yabapmatt.com:2001"              // yabapmatt
+        "sn1.steemit.com:2001",                // Steemit, Inc.
+        "sn2.steemit.com:2001",                // Steemit, Inc.
+        "sn3.steemit.com:2001",                // Steemit, Inc.
+        "sn4.steemit.com:2001",                // Steemit, Inc.
+        "sn5.steemit.com:2001",                // Steemit, Inc.
+        "sn6.steemit.com:2001",                // Steemit, Inc.
+        "seed.justyy.com:2001",                // justyy
+        "steem-seed.61bts.com:2001",           // ety001
+        "steem-seed.urbanpedia.com:2001",      // fuli
+        "api.steems.top:2001",                 // maiyude
+        "steemseed-se.privex.io:2001"          // privex(se)
+
 };
 #endif
 
