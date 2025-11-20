60631049e8cf807011f384aee0251fc635179cd3
justyy <dr.zhihua.lai@gmail.com>
2025-11-20 23:42:34 +0000

    Debug Fix

--- tests/scripts/run_unit_tests.sh
+++ tests/scripts/run_unit_tests.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
-set -x  # exit on first failure
+set -e  # exit on first failure
 
 ## default PATH is /usr/local/steemd/bin/
 BIN_PATH=${BIN_PATH:-/usr/local/steemd/bin}
+BUILD_PATH=${BUILD_PATH:-/usr/local/src/build}
+
 echo "Running unit tests (BIN_PATH=${BIN_PATH})..."
+ls -lh "${BIN_PATH}"
 
 tests_to_run=(
     "test_fixed_string"
@@ -12,16 +15,21 @@ tests_to_run=(
     "size_checker"
     "schema_test"
     "js_operation_serializer"
-    "get_dev_key "
 )
 
 for test in "${tests_to_run[@]}"; do
     echo "Running unit test: $test"
-    "${BIN_PATH}/${test} xyz wit-block-signing-0:101"
+    "${BIN_PATH}/${test}"
 done
 
+# run get_dev_key separately
+echo "Running unit test: get_dev_key"
+"${BIN_PATH}/get_dev_key" xyz "wit-block-signing-0:101"
+
 echo "Running additional unit tests with ctest..."
-cd build
+cd "${BUILD_PATH}"
+ls -lh ./
+
 ctest -j4 --output-on-failure
 ./chain_test -t basic_tests/curation_weight_test
 
